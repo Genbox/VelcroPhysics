1788a97ff4a1ad20805f107e32ca3fc01aa6defe
genbox_cp <SND\genbox_cp>
2008-08-24 21:21:16 +0000

    Deleting the XNA3 branch to recreate it

--- Branches/XNA3/Collisions/ICollideable.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FarseerGames.FarseerPhysics.Collisions {
-    interface ICollideable<T> {
-        void Collide(T t, ContactList contactList);
-    }
-}
