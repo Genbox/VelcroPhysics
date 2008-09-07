29e36b01ea1d98592d2aa060c6e61a310902d0ab
genbox_cp <SND\genbox_cp>
2008-09-07 15:28:14 +0000

    Adding multithreading sample. Not done yet

---
+++ Demos/XNA3/MultiThreading/Demos/Demo4/Demo4Screen.cs
@@ -0,0 +1,206 @@
+using System.Text;
+using FarseerGames.FarseerPhysics;
+using FarseerGames.FarseerPhysics.Collisions;
+using FarseerGames.FarseerPhysics.Dynamics;
+using FarseerGames.FarseerPhysics.Dynamics.Springs;
+using FarseerGames.FarseerPhysics.Factories;
+using FarseerGames.FarseerPhysicsDemos.Demos.DemoShare;
+using FarseerGames.FarseerPhysicsDemos.DrawingSystem;
+using FarseerGames.FarseerPhysicsDemos.ScreenSystem;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FarseerGames.FarseerPhysicsDemos.Demos.Demo4
+{
+    public class Demo4Screen : GameScreen
+    {
+        private const int pyramidBaseBodyCount = 16;
+        private readonly LineBrush _lineBrush = new LineBrush(1, Color.Black); //used to draw spring on mouse grab
+        //private Agent _agent;
+        private bool _firstRun = true;
+        private Floor _floor;
+        private FixedLinearSpring _mousePickSpring;
+        private Geom _pickedGeom;
+        private Pyramid _pyramid;
+        private Body _rectangleBody;
+        private Geom _rectangleGeom;
+        private Texture2D _rectangleTexture;
+
+        public override void Initialize()
+        {
+            PhysicsSimulator = new PhysicsSimulator(new Vector2(0, 50));
+            PhysicsSimulator.BiasFactor = .4f;
+            //for stacked objects, simultaneous collision are the bottlenecks so limit them to 2 per geometric pair.
+            PhysicsSimulator.MaxContactsToDetect = 2;
+            PhysicsSimulatorView = new PhysicsSimulatorView(PhysicsSimulator);
+
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            _lineBrush.Load(ScreenManager.GraphicsDevice);
+
+            _rectangleTexture = DrawingHelper.CreateRectangleTexture(ScreenManager.GraphicsDevice, 32, 32, 2, 0, 0,
+                                                                     Color.White, Color.Black);
+
+            _rectangleBody = BodyFactory.Instance.CreateRectangleBody(32, 32, 1f); //template              
+            _rectangleGeom = GeomFactory.Instance.CreateRectangleGeom(_rectangleBody, 32, 32); //template
+            _rectangleGeom.FrictionCoefficient = .4f;
+            _rectangleGeom.RestitutionCoefficient = 0f;
+
+            //create the _pyramid near the bottom of the screen.
+            _pyramid = new Pyramid(_rectangleBody, _rectangleGeom, 32f / 3f, 32f / 3f, 32, 32, pyramidBaseBodyCount,
+                                   new Vector2(ScreenManager.ScreenCenter.X - pyramidBaseBodyCount * .5f * (32 + 32 / 3),
+                                               ScreenManager.ScreenHeight - 125));
+            _pyramid.Load(PhysicsSimulator);
+
+            _floor = new Floor(ScreenManager.ScreenWidth, 100,
+                               new Vector2(ScreenManager.ScreenCenter.X, ScreenManager.ScreenHeight - 50));
+            _floor.Load(ScreenManager.GraphicsDevice, PhysicsSimulator);
+
+            //_agent = new Agent(ScreenManager.ScreenCenter - new Vector2(320, 300));
+            //_agent.Load(ScreenManager.GraphicsDevice, PhysicsSimulator);
+
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            if (IsActive)
+            {
+                PhysicsSimulator.Update(gameTime.ElapsedGameTime.Milliseconds * .001f);
+            }
+            base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            ScreenManager.SpriteBatch.Begin(SpriteBlendMode.AlphaBlend);
+            _pyramid.Draw(ScreenManager.SpriteBatch, _rectangleTexture);
+            _floor.Draw(ScreenManager.SpriteBatch);
+            //_agent.Draw(ScreenManager.SpriteBatch);
+
+            if (_mousePickSpring != null)
+            {
+                _lineBrush.Draw(ScreenManager.SpriteBatch,
+                                _mousePickSpring.Body.GetWorldPosition(_mousePickSpring.BodyAttachPoint),
+                                _mousePickSpring.WorldAttachPoint);
+            }
+            ScreenManager.SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public override void HandleInput(InputState input)
+        {
+            if (_firstRun)
+            {
+                ScreenManager.AddScreen(new PauseScreen(GetTitle(), GetDetails()));
+                _firstRun = false;
+            }
+
+            if (input.PauseGame)
+            {
+                ScreenManager.AddScreen(new PauseScreen(GetTitle(), GetDetails()));
+            }
+            HandleKeyboardInput(input);
+            HandleMouseInput(input);
+            base.HandleInput(input);
+        }
+
+        private void HandleKeyboardInput(InputState input)
+        {
+            const float forceAmount = 1000;
+            Vector2 force = Vector2.Zero;
+            force.Y = -force.Y;
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.A))
+            {
+                force += new Vector2(-forceAmount, 0);
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.S))
+            {
+                force += new Vector2(0, forceAmount);
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.D))
+            {
+                force += new Vector2(forceAmount, 0);
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.W))
+            {
+                force += new Vector2(0, -forceAmount);
+            }
+
+            //_agent.ApplyForce(force);
+
+            const float torqueAmount = 14000;
+            float torque = 0;
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.Left))
+            {
+                torque -= torqueAmount;
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.Right))
+            {
+                torque += torqueAmount;
+            }
+            //_agent.ApplyTorque(torque);
+        }
+
+        private void HandleMouseInput(InputState input)
+        {
+            Vector2 point = new Vector2(input.CurrentMouseState.X, input.CurrentMouseState.Y);
+            if (input.LastMouseState.LeftButton == ButtonState.Released &&
+                input.CurrentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                //create mouse spring
+                _pickedGeom = PhysicsSimulator.Collide(point);
+                if (_pickedGeom != null)
+                {
+                    _mousePickSpring = ControllerFactory.Instance.CreateFixedLinearSpring(PhysicsSimulator,
+                                                                                          _pickedGeom.Body,
+                                                                                          _pickedGeom.Body.
+                                                                                              GetLocalPosition(point),
+                                                                                          point, 20, 10);
+                }
+            }
+            else if (input.LastMouseState.LeftButton == ButtonState.Pressed &&
+                     input.CurrentMouseState.LeftButton == ButtonState.Released)
+            {
+                //destroy mouse spring
+                if (_mousePickSpring != null && _mousePickSpring.IsDisposed == false)
+                {
+                    _mousePickSpring.Dispose();
+                    _mousePickSpring = null;
+                }
+            }
+
+            //move anchor point
+            if (input.CurrentMouseState.LeftButton == ButtonState.Pressed && _mousePickSpring != null)
+            {
+                _mousePickSpring.WorldAttachPoint = point;
+            }
+        }
+
+        public string GetTitle()
+        {
+            return "Stacked Objects";
+        }
+
+        public string GetDetails()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("This demo shows the stacking stability of the engine.");
+            sb.AppendLine("It shows a stack of rectangular bodies stacked in");
+            sb.AppendLine("the shape of a _pyramid.");
+            sb.AppendLine(string.Empty);
+            sb.AppendLine("Keyboard:");
+            sb.AppendLine("  -Rotate : left and right arrows");
+            sb.AppendLine("  -Move : A,S,D,W");
+            sb.AppendLine(string.Empty);
+            sb.AppendLine("Mouse");
+            sb.AppendLine("  -Hold down left button and drag");
+            return sb.ToString();
+        }
+    }
+}
