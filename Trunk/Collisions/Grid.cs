0f1abfbc5c33553bc5fa3ae4cf8d894546b50a16
genbox_cp <SND\genbox_cp>
2008-09-29 14:43:02 +0000

    Reverted back to old grid.cs

--- Trunk/Collisions/Grid.cs
+++ Trunk/Collisions/Grid.cs
@@ -7,9 +7,6 @@ using FarseerGames.FarseerPhysics.Mathematics;
 
 namespace FarseerGames.FarseerPhysics.Collisions
 {
-    /// <summary>
-    /// Grid used for collision detection
-    /// </summary>
     public sealed class Grid
     {
         private AABB _aabb;
@@ -18,55 +15,41 @@ namespace FarseerGames.FarseerPhysics.Collisions
         private float[,] _nodes;
         private Vector2[] _points;
 
-        /// <summary>
-        /// Gets all the points that makes up the grid
-        /// </summary>
-        /// <value>The points.</value>
-        public Vector2[] GetPoints()
+        public Vector2[] Points
         {
-            return _points;
+            get { return _points; }
         }
 
-        /// <summary>
-        /// Clones this instance.
-        /// </summary>
-        /// <returns></returns>
         public Grid Clone()
         {
             Grid grid = new Grid();
             grid._gridCellSize = _gridCellSize;
             grid._gridCellSizeInv = _gridCellSizeInv;
             grid._aabb = _aabb;
-            grid._nodes = (float[,])_nodes.Clone();
-            grid._points = (Vector2[])_points.Clone();
+            grid._nodes = (float[,]) _nodes.Clone();
+            grid._points = (Vector2[]) _points.Clone();
             return grid;
         }
 
-        /// <summary>
-        /// Computes the grid. This can be timeconsuming depending on the gridCellSize
-        /// </summary>
-        /// <param name="geometry">The geometry that needs the grid calculated</param>
-        /// <param name="gridCellSize">Size of the grid cells.</param>
         public void ComputeGrid(Geom geometry, float gridCellSize)
         {
-            //Prepare the geometry.
+            //prepare the geometry.
             Matrix old = geometry.Matrix;
             Matrix identity = Matrix.Identity;
             geometry.Matrix = identity;
 
             _aabb = new AABB(geometry.AABB);
             _gridCellSize = gridCellSize;
-            _gridCellSizeInv = 1 / gridCellSize;
+            _gridCellSizeInv = 1/gridCellSize;
 
-            int xSize = (int)Math.Ceiling((double)_aabb.Width * _gridCellSizeInv) + 1;
-            int ySize = (int)Math.Ceiling((double)_aabb.Width * _gridCellSizeInv) + 1;
-
-            _nodes = new float[xSize, ySize];
-            _points = new Vector2[xSize * ySize];
-            int i = 0;
-            Vector2 vector = _aabb.Min;
+            int xSize = (int) Math.Ceiling(Convert.ToDouble((_aabb.Max.X - _aabb.Min.X)*_gridCellSizeInv)) + 1;
+            int ySize = (int) Math.Ceiling(Convert.ToDouble((_aabb.Max.Y - _aabb.Min.Y)*_gridCellSizeInv)) + 1;
 
             //TODO: Possible optimization (normal)! If the shape is symmetric in X and Y axis, don't calculate the points, replicate them.
+            _nodes = new float[xSize,ySize];
+            _points = new Vector2[xSize*ySize];
+            int i = 0;
+            Vector2 vector = _aabb.Min;
             for (int x = 0; x < xSize; ++x, vector.X += gridCellSize)
             {
                 vector.Y = _aabb.Min.Y;
@@ -81,27 +64,18 @@ namespace FarseerGames.FarseerPhysics.Collisions
             geometry.Matrix = old;
         }
 
-        /// <summary>
-        /// Check to see if it intersects with the specified vector.
-        /// Intersection is when the distance <= 0
-        /// </summary>
-        /// <param name="vector">The vector.</param>
-        /// <param name="feature">The feature.</param>
-        /// <returns>true if it intersets</returns>
         public bool Intersect(ref Vector2 vector, out Feature feature)
         {
             //TODO: Keep and eye out for floating point accuracy issues here. Possibly some
             //VERY intermittent errors exist?
-
-            //NOTE: This bug exist:
-            //http://www.codeplex.com/FarseerPhysics/Thread/View.aspx?ThreadId=34188
             if (_aabb.Contains(ref vector))
             {
-                int x = (int)Math.Floor((vector.X - _aabb.Min.X) * _gridCellSizeInv);
-                int y = (int)Math.Floor((vector.Y - _aabb.Min.Y) * _gridCellSizeInv);
+                int x = (int) Math.Floor((vector.X - _aabb.Min.X)*_gridCellSizeInv);
+                int y = (int) Math.Floor((vector.Y - _aabb.Min.Y)*_gridCellSizeInv);
 
-                float xPercent = (vector.X - (_gridCellSize * x + _aabb.Min.X)) * _gridCellSizeInv;
-                float yPercent = (vector.Y - (_gridCellSize * y + _aabb.Min.Y)) * _gridCellSizeInv;
+
+                float xPercent = (vector.X - (_gridCellSize*x + _aabb.Min.X))*_gridCellSizeInv;
+                float yPercent = (vector.Y - (_gridCellSize*y + _aabb.Min.Y))*_gridCellSizeInv;
 
                 float bottomLeft = _nodes[x, y];
                 float bottomRight = _nodes[x + 1, y];
