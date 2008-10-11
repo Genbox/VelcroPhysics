5066094632698ee7aa8e37d95ee6e4696e458e4e
genbox_cp <SND\genbox_cp>
2008-10-11 17:49:26 +0000

    Added AdvancedSamplesXNA

---
+++ Demos/AdvancedSamplesXNA/Demos/Demo2/Demo2Screen.cs
@@ -0,0 +1,189 @@
+using System.Text;
+using FarseerGames.FarseerPhysics;
+using FarseerGames.FarseerPhysics.Collisions;
+using FarseerGames.FarseerPhysics.Dynamics;
+using FarseerGames.FarseerPhysics.Dynamics.Springs;
+using FarseerGames.FarseerPhysics.Factories;
+using FarseerGames.FarseerPhysicsDemos.DrawingSystem;
+using FarseerGames.FarseerPhysicsDemos.ScreenSystem;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FarseerGames.FarseerPhysicsDemos.Demos.Demo2
+{
+    public class Demo2Screen : GameScreen
+    {
+        private LineBrush _lineBrush = new LineBrush(1, Color.Black); //used to draw spring on mouse grab
+        private Body _circleBody;
+        private Geom _circleGeom;
+        private Vector2 _circleOrigin;
+        private Texture2D _circleTexture;
+        private FixedLinearSpring _mousePickSpring;
+        private Geom _pickedGeom;
+        private Body _rectangleBody;
+        private Geom _rectangleGeom;
+        private Vector2 _rectangleOrigin;
+        private Texture2D _rectangleTexture;
+
+        public override void Initialize()
+        {
+            PhysicsSimulator = new PhysicsSimulator(new Vector2(0, 0));
+
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            _lineBrush.Load(ScreenManager.GraphicsDevice);
+
+            _rectangleTexture = DrawingHelper.CreateRectangleTexture(ScreenManager.GraphicsDevice, 128, 128, Color.Gold,
+                                                                     Color.Black);
+            _rectangleOrigin = new Vector2(_rectangleTexture.Width / 2f, _rectangleTexture.Height / 2f);
+
+            _circleTexture = DrawingHelper.CreateCircleTexture(ScreenManager.GraphicsDevice, 64, Color.White,
+                                                               Color.Black);
+            _circleOrigin = new Vector2(_circleTexture.Width / 2f, _circleTexture.Height / 2f);
+
+            _rectangleBody = BodyFactory.Instance.CreateRectangleBody(PhysicsSimulator, 128, 128, 1);
+            _rectangleBody.Position = new Vector2(256, 384);
+            _rectangleGeom = GeomFactory.Instance.CreateRectangleGeom(PhysicsSimulator, _rectangleBody, 128, 128);
+
+            _circleBody = BodyFactory.Instance.CreateCircleBody(PhysicsSimulator, 64, 1); 
+            _circleBody.Position = new Vector2(725, 384);
+            _circleGeom = GeomFactory.Instance.CreateCircleGeom(PhysicsSimulator, _circleBody, 64, 20);
+
+            base.LoadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            ScreenManager.SpriteBatch.Begin(SpriteBlendMode.AlphaBlend);
+            ScreenManager.SpriteBatch.Draw(_circleTexture, _circleGeom.Position, null, Color.White, _circleGeom.Rotation,
+                                           _circleOrigin, 1, SpriteEffects.None, 0);
+            ScreenManager.SpriteBatch.Draw(_rectangleTexture, _rectangleGeom.Position, null, Color.White,
+                                           _rectangleGeom.Rotation, _rectangleOrigin, 1, SpriteEffects.None, 0);
+
+            if (_mousePickSpring != null)
+            {
+                _lineBrush.Draw(ScreenManager.SpriteBatch,
+                                _mousePickSpring.Body.GetWorldPosition(_mousePickSpring.BodyAttachPoint),
+                                _mousePickSpring.WorldAttachPoint);
+            }
+            ScreenManager.SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public override void HandleInput(InputState input)
+        {
+            if (FirstRun)
+            {
+                ScreenManager.AddScreen(new PauseScreen(GetTitle(), GetDetails(),this));
+                FirstRun = false;
+            }
+
+            if (input.PauseGame)
+            {
+                ScreenManager.AddScreen(new PauseScreen(GetTitle(), GetDetails(),this));
+            }
+
+            HandleKeyboardInput(input);
+            HandleMouseInput(input);
+
+            base.HandleInput(input);
+        }
+
+        private void HandleKeyboardInput(InputState input)
+        {
+            const float forceAmount = 50;
+            Vector2 force = Vector2.Zero;
+            force.Y = -force.Y;
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.A))
+            {
+                force += new Vector2(-forceAmount, 0);
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.S))
+            {
+                force += new Vector2(0, forceAmount);
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.D))
+            {
+                force += new Vector2(forceAmount, 0);
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.W))
+            {
+                force += new Vector2(0, -forceAmount);
+            }
+
+            _rectangleBody.ApplyForce(force);
+
+            const float torqueAmount = 1000;
+            float torque = 0;
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.Left))
+            {
+                torque -= torqueAmount;
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.Right))
+            {
+                torque += torqueAmount;
+            }
+            _rectangleBody.ApplyTorque(torque);
+        }
+
+        private void HandleMouseInput(InputState input)
+        {
+            Vector2 point = new Vector2(input.CurrentMouseState.X, input.CurrentMouseState.Y);
+            if (input.LastMouseState.LeftButton == ButtonState.Released &&
+                input.CurrentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                //create mouse spring
+                _pickedGeom = PhysicsSimulator.Collide(point);
+                if (_pickedGeom != null)
+                {
+                    _mousePickSpring = SpringFactory.Instance.CreateFixedLinearSpring(PhysicsSimulator,
+                                                                                          _pickedGeom.Body,
+                                                                                          _pickedGeom.Body.
+                                                                                              GetLocalPosition(point),
+                                                                                          point, 20, 10);
+                }
+            }
+            else if (input.LastMouseState.LeftButton == ButtonState.Pressed &&
+                     input.CurrentMouseState.LeftButton == ButtonState.Released)
+            {
+                //destroy mouse spring
+                if (_mousePickSpring != null && _mousePickSpring.IsDisposed == false)
+                {
+                    _mousePickSpring.Dispose();
+                    _mousePickSpring = null;
+                }
+            }
+
+            //move anchor point
+            if (input.CurrentMouseState.LeftButton == ButtonState.Pressed && _mousePickSpring != null)
+            {
+                _mousePickSpring.WorldAttachPoint = point;
+            }
+        }
+
+        public string GetTitle()
+        {
+            return "Two Bodies With Geom";
+        }
+
+        public string GetDetails()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("This demo shows two bodies each with a single geometry");
+            sb.AppendLine("object attached.");
+            sb.AppendLine(string.Empty);
+            sb.AppendLine("Keyboard:");
+            sb.AppendLine("  -Rotate : left and right arrows");
+            sb.AppendLine("  -Move : A,S,D,W");
+            sb.AppendLine(string.Empty);
+            sb.AppendLine("Mouse");
+            sb.AppendLine("  -Hold down left button and drag");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Demos/AdvancedSamplesXNA/Demos/DemoShare/Agent.cs
@@ -0,0 +1,152 @@
+using FarseerGames.FarseerPhysics;
+using FarseerGames.FarseerPhysics.Collisions;
+using FarseerGames.FarseerPhysics.Dynamics;
+using FarseerGames.FarseerPhysics.Factories;
+using FarseerGames.FarseerPhysicsDemos.DrawingSystem;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using FarseerGames.FarseerPhysicsDemos.Demos.Demo4;
+
+namespace FarseerGames.FarseerPhysicsDemos.Demos.DemoShare
+{
+    public class Agent
+    {
+        private Vector2 _position;
+        private Body _agentBody;
+        private Vector2 _agentCrossBeamOrigin;
+        private Texture2D _agentCrossBeamTexture;
+        private Geom[] _agentGeom;
+        private Vector2 _agentOrigin;
+        private Texture2D _agentTexture;
+
+        private CollisionCategory _collidesWith = CollisionCategory.All;
+        private CollisionCategory _collisionCategory = CollisionCategory.All;
+
+        // POINT OF INTEREST
+        // Using this for linking in the Demo4
+        private ObjectLinker[] _agentLink;
+
+        public Agent(Vector2 position)
+        {
+            _position = position;
+        }
+
+        public Body Body
+        {
+            get { return _agentBody; }
+        }
+
+        public CollisionCategory CollisionCategory
+        {
+            get { return _collisionCategory; }
+            set { _collisionCategory = value; }
+        }
+
+        public CollisionCategory CollidesWith
+        {
+            get { return _collidesWith; }
+            set { _collidesWith = value; }
+        }
+
+        public void ApplyForce(Vector2 force)
+        {
+            _agentBody.ApplyForce(force);
+        }
+
+        public void ApplyTorque(float torque)
+        {
+            _agentBody.ApplyTorque(torque);
+        }
+
+
+        public void Load(GraphicsDevice graphicsDevice, PhysicsSimulator physicsSimulator)
+        {
+            _agentTexture = DrawingHelper.CreateCircleTexture(graphicsDevice, 16, Color.Gold, Color.Black);
+            _agentOrigin = new Vector2(_agentTexture.Width/2f, _agentTexture.Height/2f);
+
+            _agentCrossBeamTexture = DrawingHelper.CreateRectangleTexture(graphicsDevice, 16, 120, Color.DarkGray,
+                                                                          Color.Black);
+            _agentCrossBeamOrigin = new Vector2(_agentCrossBeamTexture.Width/2f, _agentCrossBeamTexture.Height/2f);
+
+            _agentBody = BodyFactory.Instance.CreateRectangleBody(physicsSimulator, 80, 80, 5);
+            _agentBody.Position = _position;
+
+            _agentGeom = new Geom[7];
+            _agentGeom[0] = GeomFactory.Instance.CreateCircleGeom(physicsSimulator, _agentBody, 16, 10,
+                                                                  new Vector2(-40, -40), 0);
+            _agentGeom[0].RestitutionCoefficient = .4f;
+            _agentGeom[0].FrictionCoefficient = .2f;
+            _agentGeom[0].CollisionGroup = 1;
+            _agentGeom[0].CollisionCategories = _collisionCategory;
+            _agentGeom[0].CollidesWith = _collidesWith;
+            _agentGeom[1] = GeomFactory.Instance.CreateGeom(physicsSimulator, _agentBody, _agentGeom[0],
+                                                            new Vector2(-40, 40), 0);
+            _agentGeom[2] = GeomFactory.Instance.CreateGeom(physicsSimulator, _agentBody, _agentGeom[0],
+                                                            new Vector2(40, -40), 0);
+            _agentGeom[3] = GeomFactory.Instance.CreateGeom(physicsSimulator, _agentBody, _agentGeom[0],
+                                                            new Vector2(40, 40), 0);
+            _agentGeom[4] = GeomFactory.Instance.CreateGeom(physicsSimulator, _agentBody, _agentGeom[0],
+                                                            new Vector2(0, 0),
+                                                            0);
+
+            _agentGeom[5] = GeomFactory.Instance.CreateRectangleGeom(physicsSimulator, _agentBody, 16, 120, Vector2.Zero,
+                                                                     MathHelper.PiOver4);
+            _agentGeom[5].CollisionGroup = 1;
+            _agentGeom[5].CollisionCategories = _collisionCategory;
+            _agentGeom[5].CollidesWith = _collidesWith;
+
+            _agentGeom[6] = GeomFactory.Instance.CreateRectangleGeom(physicsSimulator, _agentBody, 16, 120, Vector2.Zero,
+                                                                     -MathHelper.PiOver4);
+            _agentGeom[6].CollisionGroup = 1;
+            _agentGeom[6].CollisionCategories = _collisionCategory;
+            _agentGeom[6].CollidesWith = _collidesWith;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // POINT OF INTEREST
+            // Use the link to draw if possible
+            if (_agentLink == null)
+            {
+                for (int i = 5; i < 7; i++)
+                {
+                    spriteBatch.Draw(_agentCrossBeamTexture, _agentGeom[i].Position, null, Color.White,
+                                     _agentGeom[i].Rotation,
+                                     _agentCrossBeamOrigin, 1, SpriteEffects.None, 0f);
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    spriteBatch.Draw(_agentTexture, _agentGeom[i].Position, null, Color.White, _agentGeom[i].Rotation,
+                                     _agentOrigin, 1, SpriteEffects.None, 0f);
+                }
+            }
+            else
+            {
+                for (int i = 5; i < 7; i++)
+                {
+                    spriteBatch.Draw(_agentCrossBeamTexture, _agentLink[i].Position, null, Color.White,
+                                     _agentLink[i].Rotation,
+                                     _agentCrossBeamOrigin, 1, SpriteEffects.None, 0f);
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    spriteBatch.Draw(_agentTexture, _agentLink[i].Position, null, Color.White, _agentLink[i].Rotation,
+                                     _agentOrigin, 1, SpriteEffects.None, 0f);
+                }
+            }
+        }
+
+        // POINT OF INTEREST
+        // Use this to link the agent's bodies to the specified processor
+        public void LinkToProcessor(PhysicsProcessor physicsProcessor)
+        {
+            _agentLink = new ObjectLinker[_agentGeom.Length];
+            for (int iGeom = 0; iGeom < _agentGeom.Length; iGeom++)
+            {
+                _agentLink[iGeom] = new ObjectLinker(_agentGeom[iGeom]);
+                physicsProcessor.AddLink(_agentLink[iGeom]);
+            }
+        }
+    }
+}
