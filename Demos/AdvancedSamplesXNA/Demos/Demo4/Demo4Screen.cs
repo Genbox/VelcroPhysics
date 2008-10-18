57c6c45f44779c3668998cdc7f63c882da5d28f9
genbox_cp <SND\genbox_cp>
2008-10-18 21:49:39 +0000

    Added texture to polygon sample in AdvancedSamples

---
+++ Demos/AdvancedSamplesXNA/Demos/Demo4/Demo4Screen.cs
@@ -0,0 +1,199 @@
+﻿using System.Text;
+using FarseerGames.AdvancedSamples.DrawingSystem;
+using FarseerGames.AdvancedSamples.ScreenSystem;
+using FarseerGames.FarseerPhysics;
+using FarseerGames.FarseerPhysics.Collisions;
+using FarseerGames.FarseerPhysics.Dynamics;
+using FarseerGames.FarseerPhysics.Dynamics.Springs;
+using FarseerGames.FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FarseerGames.AdvancedSamples.Demos.Demo4
+{
+    public class Demo4Screen : GameScreen
+    {
+        private Texture2D _polygonTexture;
+        private Vector2 _polygonOrigin;
+        private Body _polygonBody;
+
+        private Texture2D _circleTexture;
+        private Vector2 _circleOrigin;
+        private Body _circleBody;
+
+        private LineBrush _lineBrush = new LineBrush(1, Color.Black); //used to draw spring on mouse grab
+        private FixedLinearSpring _mousePickSpring;
+        private Geom _pickedGeom;
+
+        public override void Initialize()
+        {
+            PhysicsSimulator = new PhysicsSimulator(new Vector2(0, 0));
+            PhysicsSimulatorView = new PhysicsSimulatorView(PhysicsSimulator);
+
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            //load texture that will visually represent the physics body
+            _polygonTexture = ScreenManager.ContentManager.Load<Texture2D>("Content/texture");
+
+            //Create an array to hold the data from the texture
+            uint[] data = new uint[_polygonTexture.Width * _polygonTexture.Height];
+
+            //Transfer the texture data to the array
+            _polygonTexture.GetData(data);
+
+            //Calculate the vertices from the array
+            Vertices verts = Vertices.CreatePolygon(data, _polygonTexture.Width, _polygonTexture.Height);
+
+            //Make sure that the origin of the texture is the centroid (REAL center of geometry)
+            _polygonOrigin = verts.GetCentroid();
+
+            //use the body factory to create the physics body
+            _polygonBody = BodyFactory.Instance.CreatePolygonBody(PhysicsSimulator, verts, 5);
+            _polygonBody.Position = new Vector2(500, 400);
+
+            GeomFactory.Instance.CreatePolygonGeom(PhysicsSimulator, _polygonBody, verts, 0);
+
+            _circleTexture = DrawingHelper.CreateCircleTexture(ScreenManager.GraphicsDevice, 35, Color.Gold, Color.Black);
+            _circleOrigin = new Vector2(_circleTexture.Width / 2f, _circleTexture.Height / 2f);
+            _circleBody = BodyFactory.Instance.CreateCircleBody(PhysicsSimulator, 35, 1);
+            _circleBody.Position = new Vector2(300, 400);
+
+            GeomFactory.Instance.CreateCircleGeom(PhysicsSimulator, _circleBody, 35, 20, 0);
+            _lineBrush.Load(ScreenManager.GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            ScreenManager.SpriteBatch.Begin(SpriteBlendMode.AlphaBlend);
+            ScreenManager.SpriteBatch.Draw(_polygonTexture, _polygonBody.Position, null, Color.White,
+                                           _polygonBody.Rotation, _polygonOrigin, 1, SpriteEffects.None, 0);
+            ScreenManager.SpriteBatch.Draw(_circleTexture, _circleBody.Position, null, Color.White,
+                               _circleBody.Rotation, _circleOrigin, 1, SpriteEffects.None, 0);
+
+            if (_mousePickSpring != null)
+            {
+                _lineBrush.Draw(ScreenManager.SpriteBatch,
+                                _mousePickSpring.Body.GetWorldPosition(_mousePickSpring.BodyAttachPoint),
+                                _mousePickSpring.WorldAttachPoint);
+            }
+
+            ScreenManager.SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public override void HandleInput(InputState input)
+        {
+            if (FirstRun)
+            {
+                ScreenManager.AddScreen(new PauseScreen(GetTitle(), GetDetails(), this));
+                FirstRun = false;
+            }
+            if (input.PauseGame)
+            {
+                ScreenManager.AddScreen(new PauseScreen(GetTitle(), GetDetails(), this));
+            }
+            else
+            {
+                HandleKeyboardInput(input);
+                HandleMouseInput(input);
+            }
+            base.HandleInput(input);
+        }
+
+        private void HandleKeyboardInput(InputState input)
+        {
+            const float forceAmount = 50;
+            Vector2 force = Vector2.Zero;
+            force.Y = -force.Y;
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.A))
+            {
+                force += new Vector2(-forceAmount, 0);
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.S))
+            {
+                force += new Vector2(0, forceAmount);
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.D))
+            {
+                force += new Vector2(forceAmount, 0);
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.W))
+            {
+                force += new Vector2(0, -forceAmount);
+            }
+
+            _polygonBody.ApplyForce(force);
+
+            const float torqueAmount = 1000;
+            float torque = 0;
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.Left))
+            {
+                torque -= torqueAmount;
+            }
+            if (input.CurrentKeyboardState.IsKeyDown(Keys.Right))
+            {
+                torque += torqueAmount;
+            }
+            _polygonBody.ApplyTorque(torque);
+        }
+
+        private void HandleMouseInput(InputState input)
+        {
+            Vector2 point = new Vector2(input.CurrentMouseState.X, input.CurrentMouseState.Y);
+            if (input.LastMouseState.LeftButton == ButtonState.Released &&
+                input.CurrentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                //create mouse spring
+                _pickedGeom = PhysicsSimulator.Collide(point);
+                if (_pickedGeom != null)
+                {
+                    _mousePickSpring = SpringFactory.Instance.CreateFixedLinearSpring(PhysicsSimulator,
+                                                                                      _pickedGeom.Body,
+                                                                                      _pickedGeom.Body.
+                                                                                          GetLocalPosition(point),
+                                                                                      point, 20, 10);
+                }
+            }
+            else if (input.LastMouseState.LeftButton == ButtonState.Pressed &&
+                     input.CurrentMouseState.LeftButton == ButtonState.Released)
+            {
+                //destroy mouse spring
+                if (_mousePickSpring != null && _mousePickSpring.IsDisposed == false)
+                {
+                    _mousePickSpring.Dispose();
+                    _mousePickSpring = null;
+                }
+            }
+
+            //move anchor point
+            if (input.CurrentMouseState.LeftButton == ButtonState.Pressed && _mousePickSpring != null)
+            {
+                _mousePickSpring.WorldAttachPoint = point;
+            }
+        }
+
+
+        public string GetTitle()
+        {
+            return "A Single Body";
+        }
+
+        public string GetDetails()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("This demo shows how to map vertices from a texture");
+            sb.AppendLine(string.Empty);
+            sb.AppendLine("Keyboard:");
+            sb.AppendLine("  -Rotate : left and right arrows");
+            sb.AppendLine("  -Move : A,S,D,W");
+            return sb.ToString();
+        }
+    }
+}
