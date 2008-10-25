08727aa6e3318481f2bd98b3225ce250f4ef31f1
Matthew Bettcher <mattbettcher@gmail.com>
2008-10-25 01:48:59 +0000

    Added a few variations on the chain factory.

--- Trunk/Factories/ComplexFactory.cs
+++ Trunk/Factories/ComplexFactory.cs
@@ -37,6 +37,33 @@ namespace FarseerGames.FarseerPhysics.Factories
             }
         }
 
+        public Path CreateChain(PhysicsSimulator ps, Vector2 start, Vector2 end, float width, float height, float mass)
+        {
+            Path p = CreateChain(start, end, width, height, mass, false, false);
+
+            p.AddToPhysicsSimulator(ps);
+
+            return p;
+        }
+
+        public Path CreateChain(PhysicsSimulator ps, Vector2 start, Vector2 end, float width, float height, float mass, bool pinStart, bool pinEnd)
+        {
+            Path p = CreateChain(start, end, width, height, mass, pinStart, pinEnd);
+
+            p.AddToPhysicsSimulator(ps);
+
+            return p;
+        }
+
+        public Path CreateChain(Vector2 start, Vector2 end, float width, float height, float mass)
+        {
+            Path p;
+
+            p = CreateChain(start, end, width, height, mass, false, false);
+
+            return p;
+        }
+
         public Path CreateChain(Vector2 start, Vector2 end, float width, float height, float mass, bool pinStart, bool pinEnd)
         {
             bool flip = true;
@@ -72,163 +99,6 @@ namespace FarseerGames.FarseerPhysics.Factories
                 p.Add(JointFactory.Instance.CreateFixedRevoluteJoint(p.Bodies[p.Bodies.Count - 1], p.ControlPoints[2]));
 
             return (p);
-        }
-
-        /// <summary>
-        /// Creates a chain with the specified number of links.
-        /// </summary>
-        /// <param name="physicsSimulator">The physics simulator.</param>
-        /// <param name="startPosition">The start position.</param>
-        /// <param name="endPosition">The end position.</param>
-        /// <param name="links">The number of links.</param>
-        public void CreateChain(PhysicsSimulator physicsSimulator, Vector2 startPosition, Vector2 endPosition, int links)
-        {
-            Vector2 delta = endPosition - startPosition;
-            float length = delta.Length(); // length of chain
-            delta.Normalize();
-            float angle = (float)Math.Cos(delta.X / length);
-            delta *= (length / links);
-            Body a = null, b;
-            Geom g;
-            PinJoint r;
-
-            for (int i = 0; i < links; i++)
-            {
-                b = BodyFactory.Instance.CreateRectangleBody(physicsSimulator, length / links, 10, 1);
-                b.Position = startPosition + (delta * i);
-                b.Rotation = angle;
-                g = GeomFactory.Instance.CreateRectangleGeom(physicsSimulator, b, length / links, 10);
-                g.CollisionCategories = CollisionCategory.Cat2;
-                g.CollisionGroup = 2;
-
-                if (i >= 1)
-                {
-                    r = JointFactory.Instance.CreatePinJoint(physicsSimulator, a, new Vector2((length / links) / 2, 0), b,
-                                                             new Vector2(-(length / links) / 2, 0));
-                    r.TargetDistance = 0;
-                }
-                a = b;
-            }
-        }
-
-        /// <summary>
-        /// Creates a chain with the specified width and length
-        /// </summary>
-        /// <param name="physicsSimulator">The physics simulator.</param>
-        /// <param name="startPosition">The start position.</param>
-        /// <param name="endPosition">The end position.</param>
-        /// <param name="linkLength">Length of the link.</param>
-        /// <param name="linkWidth">Width of the link.</param>
-        /// <param name="createGeometry">if set to <c>true</c> [create geometry].</param>
-        /// <param name="chainCollidesWithSelf">if set to <c>true</c> [chain collides with self].</param>
-        public void CreateChain(PhysicsSimulator physicsSimulator, Vector2 startPosition, Vector2 endPosition, float linkLength, float linkWidth,
-                                 bool createGeometry, bool chainCollidesWithSelf)
-        {
-            //Get the target length of the chain
-            float length = (startPosition - endPosition).Length() * 1.4f;
-            int numOfLinks = (int)((length / linkLength) * 1.0f);
-
-            //Find how many links we will make based on the target length and desired link length
-            float sideLinkWidth = linkWidth * 0.33333f;
-
-            //Side link is 1/3 of flat link width NOTE: we may make this a static coefficient
-            Vector2 deltaVec;
-
-            //Here we must get a delta vector 
-            if (startPosition.X < endPosition.X)
-                deltaVec.X = endPosition.X - startPosition.X;
-            else
-                deltaVec.X = startPosition.X - endPosition.X;
-
-            if (startPosition.Y < endPosition.Y)
-                deltaVec.Y = endPosition.Y - startPosition.Y;
-            else
-                deltaVec.Y = startPosition.Y - endPosition.Y;
-
-            //Find the angle of the chain in radians
-            float angle = (float)Math.Cos(deltaVec.X / length);
-
-            //Normalize our delta vector
-            deltaVec.Normalize();
-
-            //Scale delta vector
-            deltaVec *= (length / numOfLinks) * 0.75f;
-
-            Body a = null, b, c; // temp bodies
-            Geom g; // temp geom
-            PinJoint r; // temp pin joint
-
-            for (int i = 0; i < numOfLinks; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    b = BodyFactory.Instance.CreateRectangleBody(physicsSimulator, linkLength, linkWidth, 0.5f);
-                    b.Position = startPosition + (deltaVec * i);
-                    b.Rotation = angle;
-
-                    if (createGeometry)
-                    {
-                        g = GeomFactory.Instance.CreateRectangleGeom(physicsSimulator, b, linkLength, linkWidth);
-
-                        if (chainCollidesWithSelf)
-                        {
-                            g.CollisionCategories = CollisionCategory.Cat10;
-                        }
-                        else
-                        {
-                            g.CollisionCategories = CollisionCategory.Cat10;
-                            g.CollisionGroup = 1;
-                        }
-                    }
-                }
-                else
-                {
-                    b = BodyFactory.Instance.CreateRectangleBody(physicsSimulator, linkLength, sideLinkWidth, 0.5f);
-                    b.Position = startPosition + (deltaVec * i);
-                    b.Rotation = angle;
-
-                    if (createGeometry)
-                    {
-                        g = GeomFactory.Instance.CreateRectangleGeom(physicsSimulator, b, linkLength, sideLinkWidth);
-
-                        if (chainCollidesWithSelf)
-                        {
-                            g.CollisionEnabled = false;
-                        }
-                        else
-                        {
-                            g.CollisionCategories = CollisionCategory.Cat10;
-                            g.CollisionGroup = 1;
-                        }
-                    }
-                }
-
-                //For testing only
-                if (i == 0)
-                    JointFactory.Instance.CreateFixedRevoluteJoint(physicsSimulator, b, startPosition);
-                if (i == (numOfLinks - 1))
-                {
-                    c = BodyFactory.Instance.CreateCircleBody(physicsSimulator, 50.0f, 1.0f);
-                    c.Position = endPosition;
-                    g = GeomFactory.Instance.CreateCircleGeom(physicsSimulator, c, 50, 36);
-                    g.CollisionEnabled = true;
-                    g.CollisionCategories = CollisionCategory.Cat10;
-                    g.CollisionGroup = 1;
-                    JointFactory.Instance.CreateRevoluteJoint(physicsSimulator, c, b, startPosition + (deltaVec * i));
-                }
-
-                //We have passed the first link
-                if (i >= 1 && i < (numOfLinks))
-                {
-                    r = JointFactory.Instance.CreatePinJoint(physicsSimulator, a, new Vector2((linkLength / 2.0f) * 0.75f, 0), b,
-                                                             new Vector2(-(linkLength / 2.0f) * 0.75f, 0));
-                    r.TargetDistance = 0;
-                    r.BiasFactor = 0.1f;
-                    r.Softness = 0.1f;
-                    r.Breakpoint = linkLength;
-                }
-                a = b;
-            }
         }
     }
 }
