671d7e8ee1427d5f8d8a0f6044126babcc177343
Ian Qvist <Genbox@users.noreply.github.com>
2008-11-01 22:49:59 +0000

    Updating Silverlight manual to have a more loose linear spring demo

--- Documentation/FarseerSilverlightManual/Demos/LinearSpringDemo.cs
+++ Documentation/FarseerSilverlightManual/Demos/LinearSpringDemo.cs
@@ -17,14 +17,14 @@ namespace FarseerSilverlightManual.Demos
         public override void Initialize()
         {
             Body rectangleBody = BodyFactory.Instance.CreateRectangleBody(physicsSimulator, 128, 128, 1);
-            rectangleBody.Position = new Vector2(ScreenManager.ScreenWidth/2f, 200);
+            rectangleBody.Position = new Vector2(ScreenManager.ScreenWidth / 2f, 200);
 
             Geom rectangleGeom = GeomFactory.Instance.CreateRectangleGeom(physicsSimulator, rectangleBody, 128, 128);
             rectangleGeom.CollisionGroup = 1;
             AddRectangleToCanvas(rectangleBody, new Vector2(128, 128));
 
             Body rectangleBody1 = BodyFactory.Instance.CreateRectangleBody(physicsSimulator, 128, 128, 1);
-            rectangleBody1.Position = new Vector2(ScreenManager.ScreenWidth/2f, 300);
+            rectangleBody1.Position = new Vector2(ScreenManager.ScreenWidth / 2f, 400);
 
             Geom rectangleGeom1 = GeomFactory.Instance.CreateRectangleGeom(physicsSimulator, rectangleBody1, 128, 128);
             rectangleGeom1.CollisionGroup = 2;
@@ -32,8 +32,9 @@ namespace FarseerSilverlightManual.Demos
 
             LinearSpring linearSpring = SpringFactory.Instance.CreateLinearSpring(physicsSimulator, rectangleBody,
                                                                                   Vector2.Zero, rectangleBody1,
-                                                                                  Vector2.Zero, 10f, 20f);
-            linearSpring.RestLength = 10;
+                                                                                  Vector2.Zero, 10f, 1f);
+            linearSpring.RestLength = 100;
+
             AddLinearSpringBrushToCanvas(linearSpring);
             base.Initialize();
         }
