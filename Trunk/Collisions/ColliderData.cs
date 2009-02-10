e75f0448af9c41065544accb85f2eadfb4d4f2b1
genbox_cp <SND\genbox_cp>
2009-02-10 22:50:33 +0000

    Adding forgotten files to their right places

---
+++ Trunk/Collisions/ColliderData.cs
@@ -0,0 +1,16 @@
+﻿namespace FarseerGames.FarseerPhysics.Collisions
+{
+    public struct ColliderData
+    {
+        public float GridCellSize;
+        public static ColliderData DefaultSettings
+        {
+            get
+            {
+                ColliderData data = new ColliderData();
+                data.GridCellSize = 0;
+                return data;
+            }
+        }
+    }
+}
