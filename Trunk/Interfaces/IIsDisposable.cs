47ec34b84c3120bf94b376121ab5c10cfc0cdab3
Ian Qvist <Genbox@users.noreply.github.com>
2009-02-10 22:50:33 +0000

    Adding forgotten files to their right places

---
+++ Trunk/Interfaces/IIsDisposable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FarseerGames.FarseerPhysics.Interfaces
+{
+    /// <summary>
+    /// Interface used by the generic list
+    /// </summary>
+    public interface IIsDisposable : IDisposable
+    {
+        bool IsDisposed { get; set; }
+    }
+}
