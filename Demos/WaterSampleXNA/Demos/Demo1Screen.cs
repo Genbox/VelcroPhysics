0f9189b8f425d3545ab29dbb01f928561ddf5b17
genbox_cp <SND\genbox_cp>
2009-04-08 16:22:18 +0000

    Adding XNA Water sample. Not fully working yet.

---
+++ Demos/WaterSampleXNA/Demos/Demo1Screen.cs
@@ -0,0 +1,166 @@
+using System.Text;
+using FarseerGames.FarseerPhysics;
+using FarseerGames.FarseerPhysics.Collisions;
+using FarseerGames.FarseerPhysics.Dynamics;
+using FarseerGames.FarseerPhysics.Factories;
+using FarseerGames.FarseerPhysics.Mathematics;
+using FarseerGames.SimpleSamples.Demos.Demo4;
+using FarseerGames.SimpleSamples.Demos.DemoShare;
+using FarseerGames.SimpleSamples.DrawingSystem;
+using FarseerGames.SimpleSamples.ScreenSystem;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using WaterSampleXNA.Demos;
+
+namespace FarseerGames.SimpleSamples.Demos.Demo1
+{
+    public class Demo1Screen : GameScreen
+    {
+        private Box _platform;
+        private Body _refBody;
+        private Geom _refGeom;
+        private Pyramid _pyramid;
+        private Texture2D _refTexture;
+        private const int _pyramidBaseBodyCount = 5;
+        private Matrix _cameraMatrix;
+        private Matrix _projectionMatrix;
+        private Matrix _worldMatrix;
+        private VertexPositionColor[] _vertices; // an array of vertices with position and color
+        private WaterModel _waterModel;
+
+        public override void Initialize()
+        {
+            PhysicsSimulator = new PhysicsSimulator(new Vector2(0, 150));
+            PhysicsSimulatorView = new PhysicsSimulatorView(PhysicsSimulator);
+
+            //_cameraMatrix = Matrix.Identity;
+            //_projectionMatrix = Matrix.CreateOrthographicOffCenter(0, 800, 600, 0, -100, 100);
+            //_worldMatrix = Matrix.Identity;
+
+            _waterModel = new WaterModel();
+            _waterModel.Initialize(PhysicsSimulator);
+
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            _platform = new Box(300, 20, 5, new Vector2(ScreenManager.ScreenWidth / 2f, 600), Color.White, Color.Black, 1);
+            _platform.Load(ScreenManager.GraphicsDevice, PhysicsSimulator);
+
+            _refTexture = DrawingHelper.CreateRectangleTexture(ScreenManager.GraphicsDevice, 32, 32, 2, 0, 0,
+                                                         Color.White, Color.Black);
+
+            _refBody = BodyFactory.Instance.CreateRectangleBody(32, 32, 1);
+            _refGeom = GeomFactory.Instance.CreateRectangleGeom(_refBody, 32, 32);
+            _refGeom.FrictionCoefficient = .2f;
+
+            //create the _pyramid near the bottom of the screen.
+            _pyramid = new Pyramid(_refBody, _refGeom, 32f / 3f, 32f / 3f, 32, 32, _pyramidBaseBodyCount,
+                                   new Vector2(ScreenManager.ScreenCenter.X - _pyramidBaseBodyCount * .5f * (32 + 32 / 3) + 20,
+                                               ScreenManager.ScreenHeight - 200));
+
+            _pyramid.Load(PhysicsSimulator);
+
+            //Add the geometries to the watermodel's fluiddragcontroller to make them float.
+            _waterModel.FluidDragController.AddGeom(_platform.Geom);
+
+            foreach (Geom geom in _pyramid.Geoms)
+            {
+                _waterModel.FluidDragController.AddGeom(geom);
+            }
+
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            _waterModel.Update(gameTime.ElapsedGameTime);
+
+            //_gameModel.Update(gameTime.ElapsedGameTime);
+            //_waterModel.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+
+            base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+        }
+
+
+        public override void Draw(GameTime gameTime)
+        {
+            ScreenManager.SpriteBatch.Begin(SpriteBlendMode.AlphaBlend);
+            _pyramid.Draw(ScreenManager.SpriteBatch, _refTexture);
+            _platform.Draw(ScreenManager.SpriteBatch);
+            ScreenManager.SpriteBatch.End();
+
+            ScreenManager.GraphicsDevice.RenderState.CullMode = CullMode.None;
+
+            // create the triangle strip
+            CreateWaterVertexBuffer();
+
+            // set the devices vertex declaration
+            ScreenManager.GraphicsDevice.VertexDeclaration = new VertexDeclaration(ScreenManager.GraphicsDevice,
+                                                                              VertexPositionColor.VertexElements);
+
+            // create a basic effect with no pool
+            BasicEffect effect = new BasicEffect(ScreenManager.GraphicsDevice, null);
+
+            // set the effects matrix's
+            effect.World = _worldMatrix;
+            effect.View = _cameraMatrix;
+            effect.Projection = _projectionMatrix;
+
+            effect.Alpha = 0.5f;
+
+            effect.VertexColorEnabled = true;
+            effect.Begin();
+
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Begin();
+                ScreenManager.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, _vertices, 0,
+                                                           _waterModel.WaveController.NodeCount);
+                pass.End();
+            }
+
+            base.Draw(gameTime);
+        }
+
+        public void CreateWaterVertexBuffer()
+        {
+            _vertices = new VertexPositionColor[22];
+
+            // start at bottom left
+            Vector3 position = new Vector3(0, 600, 0);
+            _vertices[0] = new VertexPositionColor(position, Color.Aquamarine); // save it to the buffer
+
+            // for each point on the wave
+            for (int i = 0; i <= _waterModel.WaveController.NodeCount - 1; i += 2)
+            {
+                position = new Vector3(_waterModel.WaveController.XPosition[i],
+                                       600 - _waterModel.WaveController.Height + _waterModel.WaveController.CurrentWave[i], 0);
+                // bottom of screen - waveHeight + that nodes offset
+                _vertices[i + 1] = new VertexPositionColor(position, Color.Aquamarine); // save it to the buffer
+
+                position = new Vector3(_waterModel.WaveController.XPosition[i + 1], 600, 0); // bottom of screen
+                _vertices[i + 2] = new VertexPositionColor(position, Color.Aquamarine); // save it to the buffer
+            }
+
+            // start at bottom left
+            position = new Vector3(800, 600 - _waterModel.WaveController.Height, 0); // assumes bottom of screen is 600
+            _vertices[_waterModel.WaveController.NodeCount + 1] = new VertexPositionColor(position, Color.Aquamarine);
+            // save it to the buffer
+        }
+
+        public static string GetTitle()
+        {
+            return "Start water sample";
+        }
+
+        public static string GetDetails()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("This demo shows how to do water in XNA");
+            sb.AppendLine(string.Empty);
+            return sb.ToString();
+        }
+    }
+}
---
+++ Demos/WaterSampleXNA/Demos/WaterModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using FarseerGames.FarseerPhysics;
+using FarseerGames.FarseerPhysics.Controllers;
+using FarseerGames.FarseerPhysics.Mathematics;
+using Microsoft.Xna.Framework;
+
+namespace WaterSampleXNA.Demos
+{
+    /// <summary>
+    ///The water model in this demo is made up of two parts.  
+    ///
+    ///The wave controller controls the motion of the wave. It can control
+    ///the size of the wave, the speed, the damping, etc..
+    ///
+    ///The fluid drag controller controls the forces that act upon 
+    ///bodies that are IN the water.  It controls how thing float,
+    ///the fluid drag, etc..
+    ///
+    ///Each of these controller could be used by themselves, but 
+    ///combining them gives the most full featured water simulation.
+    /// </summary>
+    public class WaterModel
+    {
+        public WaveController WaveController { get; private set; }
+        public FluidDragController FluidDragController { get; private set; }
+
+        public float WaveGeneratorMax { get; set; }
+        public float WaveGeneratorMin { get; set; }
+        public float WaveGeneratorStep { get; set; }
+
+        public WaterModel()
+        {
+            WaveGeneratorMax = 0.20f; //initial value
+            WaveGeneratorMin = 0.05f; //initial value
+            WaveGeneratorStep = 0f;
+        }
+
+        public void Initialize(PhysicsSimulator physicsSimulator)
+        {
+            //The wave controller controls how the waves move.. how big, how fast, etc..
+            //The wave controller is represented as set of points equally
+            //spaced horizontally along the width of the wave.
+            WaveController = new WaveController();
+            WaveController.Position = new Vector2(0, 500);
+            WaveController.Width = 900;
+            WaveController.Height = 100;
+            WaveController.NodeCount = 20; //how many vertices make up the surface of the wave
+            WaveController.DampingCoefficient = .95f; //determines how quickly the wave will disipate
+            WaveController.Frequency = .16f; //determines how fast the wave algorithm runs (seconds)
+
+            //The wave generator parameters simply move an end-point of the wave up and down.
+            //Think of a string attached to a wall on one end and held by a person on the other.
+            //If the person moves the string up and down to make "waves" then the arm is acting
+            //similar to the wave generator. The WaveGeneratorStep property controls how fast the "arm"
+            //moves.
+            WaveController.WaveGeneratorMax = WaveGeneratorMax;
+            WaveController.WaveGeneratorMin = WaveGeneratorMin;
+            WaveController.WaveGeneratorStep = WaveGeneratorStep;
+
+            WaveController.Initialize();
+
+            //fluid drag controller controls how things move once IN the water.
+            FluidDragController = new FluidDragController();
+            FluidDragController.Initialize(WaveController, 0.005f, 0.004f, 0.002f, physicsSimulator.Gravity);
+            //init with default values.
+            physicsSimulator.Add(FluidDragController);
+        }
+
+        public void Reset()
+        {
+            FluidDragController.Reset();
+        }
+
+        public void Update(TimeSpan elapsedTime)
+        {
+            WaveController.Update((float)elapsedTime.TotalSeconds);
+        }
+    }
+}
