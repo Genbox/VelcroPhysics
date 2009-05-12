7eb2ca84a3991f0ee5cb058a854cda131d7337db
mattbettcher_cp <SND\mattbettcher_cp>
2009-05-12 01:55:51 +0000

    Added caching to PolygonBrush, now quite fast, still no boarders. Added a simulation feature to the boolean polygon demo (8).

--- Demos/AdvancedSamplesXNA/DrawingSystem/PolygonBrush.cs
+++ Demos/AdvancedSamplesXNA/DrawingSystem/PolygonBrush.cs
@@ -11,9 +11,12 @@ namespace FarseerGames.AdvancedSamplesXNA.DrawingSystem
         private float _layer;
         private float _alpha = 1.0f;
         private Vertices _vertices;
-        private Vector3[] _coloredVertices;
         private BasicEffect _effect;
         private GraphicsDevice _graphicsDevice;
+        private Vector2[] _triangulatedVertices;
+        private VertexPositionColor[] _shaderVertices;
+        private int[] _indices;
+            
 
         public PolygonBrush()
         {
@@ -59,11 +62,14 @@ namespace FarseerGames.AdvancedSamplesXNA.DrawingSystem
             set
             {
                 _vertices = value;
-                _coloredVertices = new Vector3[_vertices.Count];
+                _shaderVertices = new VertexPositionColor[value.Count];
+
+                Vertices.Triangulate(_vertices.GetVerticesArray(), Vertices.WindingOrder.CounterClockwise, out _triangulatedVertices, out _indices);
 
                 for (int i = 0; i < _vertices.Count; i++)
                 {
-                    _coloredVertices[i] = new Vector3(_vertices[i].X, _vertices[i].Y, _layer);
+                    _shaderVertices[i].Position = new Vector3(_triangulatedVertices[i].X, _triangulatedVertices[i].Y, _layer);
+                    _shaderVertices[i].Color = _color;
                 }
             }
         }
@@ -76,32 +82,21 @@ namespace FarseerGames.AdvancedSamplesXNA.DrawingSystem
 
         public void Draw(Vector2 position, float rotation)
         {
-            Vector2[] triangulatedVertices;
-            VertexPositionColor[] shaderVertices = new VertexPositionColor[_vertices.Count];
-            int[] indices;
-            
             Matrix cameraMatrix = Matrix.Identity;
             Matrix projectionMatrix = Matrix.CreateOrthographicOffCenter(0, _graphicsDevice.Viewport.Width, _graphicsDevice.Viewport.Height, 0, -100, 100);
             Matrix worldMatrix = Matrix.CreateRotationZ(rotation);
             worldMatrix *= Matrix.CreateTranslation(new Vector3(position, 0));
 
-            Vertices.Triangulate(_vertices.GetVerticesArray(), Vertices.WindingOrder.CounterClockwise, out triangulatedVertices, out indices);
-
-            for (int i = 0; i < triangulatedVertices.Length; i++)
-            {
-                shaderVertices[i].Position = new Vector3(triangulatedVertices[i].X, triangulatedVertices[i].Y, 0.5f);
-                shaderVertices[i].Color = _color;
-            }
 
             // set the effects matrix's
             _effect.World = worldMatrix;
             _effect.View = cameraMatrix;
             _effect.Projection = projectionMatrix;
 
-            //_effect.Alpha = _alpha;    // this effect supports a blending mode
+            _effect.Alpha = _alpha;    // this effect supports a blending mode
 
-            //_effect.VertexColorEnabled = true;   // we must enable vertex coloring with this effect
-            _effect.GraphicsDevice.RenderState.CullMode = CullMode.None;
+            _effect.VertexColorEnabled = true;   // we must enable vertex coloring with this effect
+            _effect.GraphicsDevice.RenderState.CullMode = CullMode.CullCounterClockwiseFace;
 
             _effect.Begin();
 
@@ -110,7 +105,7 @@ namespace FarseerGames.AdvancedSamplesXNA.DrawingSystem
                 _effect.DiffuseColor = _color.ToVector3();
                 pass.Begin();
                 //_graphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleFan, _coloredVertices, 0, _coloredVertices.Length - 2);
-                _graphicsDevice.DrawUserIndexedPrimitives<VertexPositionColor>(PrimitiveType.TriangleList, shaderVertices, 0, triangulatedVertices.Length, indices, 0, indices.Length / 3);
+                _graphicsDevice.DrawUserIndexedPrimitives<VertexPositionColor>(PrimitiveType.TriangleList, _shaderVertices, 0, _triangulatedVertices.Length, _indices, 0, _indices.Length / 3);
                 //_effect.DiffuseColor = _borderColor.ToVector3();
                 //_effect.CommitChanges();
                 //_graphicsDevice.DrawUserPrimitives(PrimitiveType.LineStrip, _coloredVertices, 0, _coloredVertices.Length - 1);
