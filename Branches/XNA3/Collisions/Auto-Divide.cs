d4483b731012873a167825a402c215ef9406ef60
Matthew Bettcher <mattbettcher@gmail.com>
2009-05-30 21:20:00 +0000

    Added Auto-divide with support for triangulation of geoms. Added Demo 10 in Advanced Samples - shows how to use Auto-divide to create concave shapes and still use SAT.

---
+++ Branches/XNA3/Collisions/Auto-Divide.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using FarseerGames.FarseerPhysics.Collisions;
+using FarseerGames.FarseerPhysics.Factories;
+using FarseerGames.FarseerPhysics.Dynamics;
+
+#if (XNA)
+using Microsoft.Xna.Framework;
+#else
+using FarseerGames.FarseerPhysics.Mathematics;
+#endif
+
+namespace FarseerGames.FarseerPhysics.Collisions
+{
+    /// <summary>
+    /// Splits large geoms into multiple smaller geoms for better performance.
+    /// </summary>
+    public static class AutoDivide
+    {
+        public static List<Vertices> DivideVertices(Vertices v, bool createTriangles)
+        {
+            List<Vertices> split = new List<Vertices>();   // a list holding all vertices as they're split
+            Vector2[] triangles;
+            short[] indices;
+            int j = 0;
+            Vertices temp = new Vertices();
+
+            // split the vertices up into a set of triangles
+            Vertices.Triangulate(v.ToArray(), Vertices.WindingOrder.CounterClockwise, out triangles, out indices);
+
+            if (createTriangles)
+            {
+                // for each triangle
+                for (int i = 0; i < indices.Length / 3; i++)
+                {
+                    temp.Add(triangles[indices[j]]);
+                    j++;
+                    temp.Add(triangles[indices[j]]);
+                    j++;
+                    temp.Add(triangles[indices[j]]);
+                    j++;
+                    
+                    split.Add(new Vertices(temp));
+                    temp.Clear();
+                }
+            }
+
+            return split;
+        }
+
+        public static List<Geom> DivideGeom(Vertices vertices, Body body)
+        {
+            List<Geom> geomsList = new List<Geom>();
+
+            List<Vertices> verts = AutoDivide.DivideVertices(vertices, true);
+
+            Vector2 a = vertices.GetCentroid();
+
+            foreach (Vertices v in verts)
+            {
+                if (v.Count > 0)
+                {
+                    Vector2 b = v.GetCentroid();
+
+                    geomsList.Add(GeomFactory.Instance.CreatePolygonGeom(body, v, b - a, 0.0f, 1.0f));
+                }
+            }
+            return geomsList;
+        }
+    }
+}
---
+++ Demos/AdvancedSamplesXNA/Demos/Demo10/Splat.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using FarseerGames.FarseerPhysics;
+using FarseerGames.FarseerPhysics.Collisions;
+using FarseerGames.FarseerPhysics.Dynamics;
+using FarseerGames.FarseerPhysics.Factories;
+using FarseerGames.AdvancedSamplesXNA.DrawingSystem;
+using FarseerGames.AdvancedSamplesXNA.ScreenSystem;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace FarseerGames.AdvancedSamplesXNA.Demos.Demo10
+{
+    internal class Splat
+    {
+        private List<Geom> _splatGeomList;
+        private Body _splatBody;
+
+        private static Texture2D _splatTexture;
+        private static Vertices _splatTextureVertices;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        public static void Load(ContentManager content)
+        {
+            _splatTexture = content.Load<Texture2D>("Content/Terrain");
+
+            //Create an array to hold the data from the texture
+            uint[] data = new uint[_splatTexture.Width * _splatTexture.Height];
+
+            //Transfer the texture data to the array
+            _splatTexture.GetData(data);
+
+            _splatTextureVertices = Vertices.CreatePolygon(data, _splatTexture.Width, _splatTexture.Height, new Vector2(), 128, 2.0f);
+        }
+
+        public Splat(PhysicsSimulator ps, Vector2 position)
+        {
+            _splatGeomList = new List<Geom>();
+            
+            _splatBody = BodyFactory.Instance.CreatePolygonBody(_splatTextureVertices, 1);
+            _splatBody.Position = position;
+
+            _splatGeomList = AutoDivide.DivideGeom(_splatTextureVertices, _splatBody);
+
+            ps.Add(_splatBody);
+
+            foreach (Geom g in _splatGeomList)
+            {
+                ps.Add(g);
+            }
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(_splatTexture, _splatBody.Position, null, Color.White, _splatBody.Rotation, _splatTextureVertices.GetCentroid(), 1.0f, SpriteEffects.None, 0.5f);
+        }
+    }
+}
