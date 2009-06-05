bafe19a6765dfba4001e806589dc71c61f01b54d
Matthew Bettcher <mattbettcher@gmail.com>
2009-06-05 13:54:38 +0000

    Updated Auto-Divide to include easy to use decomposition methods. Changed Splat texture to be more splaty. Updated Advanced Demo 10 to use new Auto-Divide methods.

--- Branches/XNA3/Collisions/Auto-Divide.cs
+++ Branches/XNA3/Collisions/Auto-Divide.cs
@@ -21,9 +21,8 @@ namespace FarseerGames.FarseerPhysics.Collisions
         /// triangle, from a set of vertices.
         /// </summary>
         /// <param name="vertices">The set of vertices. Must be a closed set.</param>
-        /// <param name="createTriangles">Create only triangles. (Must be true for now)</param>
         /// <returns>A list of divided vertices</returns>
-        public static List<Vertices> DivideVertices(Vertices vertices, bool createTriangles)
+        public static List<Vertices> TriangulateVertices(Vertices vertices)
         {
             List<Vertices> split = new List<Vertices>();   // a list holding all vertices as they're split
             Vector2[] triangles;
@@ -34,21 +33,18 @@ namespace FarseerGames.FarseerPhysics.Collisions
             // split the vertices up into a set of triangles
             Vertices.Triangulate(vertices.ToArray(), Vertices.WindingOrder.CounterClockwise, out triangles, out indices);
 
-            if (createTriangles)
+            // for each triangle
+            for (int i = 0; i < indices.Length / 3; i++)
             {
-                // for each triangle
-                for (int i = 0; i < indices.Length / 3; i++)
-                {
-                    temp.Add(triangles[indices[j]]);
-                    j++;
-                    temp.Add(triangles[indices[j]]);
-                    j++;
-                    temp.Add(triangles[indices[j]]);
-                    j++;
-                    
-                    split.Add(new Vertices(temp));
-                    temp.Clear();
-                }
+                temp.Add(triangles[indices[j]]);
+                j++;
+                temp.Add(triangles[indices[j]]);
+                j++;
+                temp.Add(triangles[indices[j]]);
+                j++;
+                
+                split.Add(new Vertices(temp));
+                temp.Clear();
             }
 
             return split;
@@ -61,11 +57,11 @@ namespace FarseerGames.FarseerPhysics.Collisions
         /// <param name="vertices">The set of vertices. Must be a closed set.</param>
         /// <param name="body">The body you want the geoms to belong too.</param>
         /// <returns>A list of divided geometries</returns>
-        public static List<Geom> DivideGeom(Vertices vertices, Body body)
+        public static List<Geom> TriangulateGeom(Vertices vertices, Body body)
         {
             List<Geom> geomsList = new List<Geom>();
 
-            List<Vertices> verts = DivideVertices(vertices, true);
+            List<Vertices> verts = TriangulateVertices(vertices);
 
             Vector2 a = vertices.GetCentroid();
 
@@ -79,6 +75,45 @@ namespace FarseerGames.FarseerPhysics.Collisions
                 }
             }
             return geomsList;
+        }
+
+        /// <summary>
+        /// Decomposes a set of vertices into a set of vertices.
+        /// </summary>
+        /// <param name="vertices">Vertices to decompose.</param>
+        /// <param name="maxPolysToFind">Maximum Vertices to return.</param>
+        /// <returns>A list of Vertices.</returns>
+        public static List<Vertices> DecomposeVertices(Vertices vertices, int maxPolysToFind)
+        {
+            Vertices[] verts = Polygon.DecomposeVertices(vertices, maxPolysToFind);
+
+            return new List<Vertices>(verts);
+        }
+
+
+        /// <summary>
+        /// Decomposes a set of vertices into a set of Geoms all
+        /// attached to one body.
+        /// </summary>
+        /// <param name="vertices">Vertices to decompose.</param>
+        /// <param name="body">Body to attach too.</param>
+        /// <param name="maxPolysToFind">Maximum Geoms to return.</param>
+        /// <returns>A list of Geoms.</returns>
+        public static List<Geom> DecomposeGeom(Vertices vertices, Body body, int maxPolysToFind)
+        {
+            Vertices[] verts = Polygon.DecomposeVertices(vertices, maxPolysToFind);
+
+            List<Geom> geomList = new List<Geom>();
+
+            Vector2 mainCentroid = vertices.GetCentroid();
+
+            foreach (Vertices v in verts)
+            {
+                Vector2 subCentroid = v.GetCentroid();
+                geomList.Add(new Geom(body, v, -mainCentroid, 0, 1.0f));
+            }
+
+            return geomList;
         }
     }
 }
