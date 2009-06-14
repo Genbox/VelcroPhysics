e7d0d83db2f931fdf196e592da7020273e4f5011
genbox_cp <SND\genbox_cp>
2009-06-14 21:29:01 +0000

    Adding branches for FP2

---
+++ Branches/Farseer Physics 2/Xbox360/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Farseer Physics Xbox360")]
+[assembly: AssemblyDescription("2D Physics Engine demos")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Farseer Games")]
+[assembly: AssemblyCopyright("Copyright © 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e910b874-8678-4445-9f28-1cecae6e600c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.1.0.0")]
