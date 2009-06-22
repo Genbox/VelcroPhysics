18d022c070c291ffe24869cd7ab695b0056b2d21
mattbettcher_cp <SND\mattbettcher_cp>
2009-06-22 21:11:22 +0000

    Does not compile Updated CircleContact to revision 218. Minor changes to some accesability modifiers in Contact and ContactSolver.

--- SourceFiles/Box2DX/Dynamics/Contacts/CircleContact.cs
+++ SourceFiles/Box2DX/Dynamics/Contacts/CircleContact.cs
@@ -30,102 +30,38 @@ namespace Box2DX.Dynamics
 {
 	public class CircleContact : Contact
 	{
-		public Manifold _manifold = new Manifold();
-
-		public override Manifold[] GetManifolds()
+        public CircleContact(Fixture fixtureA, Fixture fixtureB)
+            : base(fixtureA, fixtureB)
 		{
-			return new Manifold[] { _manifold };
+            Box2DXDebug.Assert(fixtureA.GetType() == ShapeType.CircleShape);
+            Box2DXDebug.Assert(fixtureB.GetType() == ShapeType.CircleShape);
 		}
 
-		public CircleContact(Shape s1, Shape s2)
-			: base(s1, s2)
+		public override void Evaluate()
 		{
-			Box2DXDebug.Assert(_shape1.GetType() == ShapeType.CircleShape);
-			Box2DXDebug.Assert(_shape2.GetType() == ShapeType.CircleShape);
-			_manifold.PointCount = 0;
-			_manifold.Points[0].NormalImpulse = 0.0f;
-			_manifold.Points[0].TangentImpulse = 0.0f;
+            Body bodyA = _fixtureA.GetBody();
+            Body bodyB = _fixtureB.GetBody();
+
+           Collision.Collision.CollideCircles(ref _manifold,
+                                (CircleShape)_fixtureA.GetShape(), bodyA.GetXForm(),
+                                (CircleShape)_fixtureB.GetShape(), bodyB.GetXForm());
 		}
 
-		public override void Evaluate(ContactListener listener)
+        public float ComputeTOI(Sweep sweepA, Sweep sweepB)
+        {
+	        TOIInput input;
+	        input.sweepA = sweepA;
+	        input.sweepB = sweepB;
+	        input.sweepRadiusA = _fixtureA.ComputeSweepRadius(sweepA.LocalCenter);
+	        input.sweepRadiusB = _fixtureB.ComputeSweepRadius(sweepB.LocalCenter);
+	        input.tolerance = Settings.LinearSlop;
+
+	        return TimeOfImpact(input, (CircleShape)_fixtureA.GetShape(), (CircleShape)_fixtureB.GetShape());
+        }
+
+		new public static Contact Create(Fixture fixtureA, Fixture fixtureB,)
 		{
-			Body b1 = _shape1.GetBody();
-			Body b2 = _shape2.GetBody();
-#warning "needfix"
-			//memcpy(&m0, &m_manifold, sizeof(b2Manifold));
-			Manifold m0 = _manifold.Clone();
-
-			Collision.Collision.CollideCircles(ref _manifold, (CircleShape)_shape1, b1.GetXForm(), 
-				(CircleShape)_shape2, b2.GetXForm());
-
-			ContactPoint cp = new ContactPoint();
-			cp.FixtureA = _shape1;
-			cp.FixtureB = _shape2;
-			cp.Friction = Settings.MixFriction(_shape1.Friction, _shape2.Friction);
-			cp.Restitution = Settings.MixRestitution(_shape1.Restitution, _shape2.Restitution);
-
-			if (_manifold.PointCount > 0)
-			{
-				_manifoldCount = 1;
-				ManifoldPoint mp = _manifold.Points[0];
-
-				if (m0.PointCount == 0)
-				{
-					mp.NormalImpulse = 0.0f;
-					mp.TangentImpulse = 0.0f;
-
-					if (listener!=null)
-					{
-						cp.Position = b1.GetWorldPoint(mp.LocalPoint1);
-						Vec2 v1 = b1.GetLinearVelocityFromLocalPoint(mp.LocalPoint1);
-						Vec2 v2 = b2.GetLinearVelocityFromLocalPoint(mp.LocalPoint2);
-						cp.Velocity = v2 - v1;
-						cp.Normal = _manifold.Normal;
-						cp.Separation = mp.Separation;
-						cp.ID = mp.ID;
-						listener.Add(cp);
-					}
-				}
-				else
-				{
-					ManifoldPoint mp0 = m0.Points[0];
-					mp.NormalImpulse = mp0.NormalImpulse;
-					mp.TangentImpulse = mp0.TangentImpulse;
-
-					if (listener!=null)
-					{
-						cp.Position = b1.GetWorldPoint(mp.LocalPoint1);
-						Vec2 v1 = b1.GetLinearVelocityFromLocalPoint(mp.LocalPoint1);
-						Vec2 v2 = b2.GetLinearVelocityFromLocalPoint(mp.LocalPoint2);
-						cp.Velocity = v2 - v1;
-						cp.Normal = _manifold.Normal;
-						cp.Separation = mp.Separation;
-						cp.ID = mp.ID;
-						listener.Persist(cp);
-					}
-				}
-			}
-			else
-			{
-				_manifoldCount = 0;
-				if (m0.PointCount > 0 && listener!=null)
-				{
-					ManifoldPoint mp0 = m0.Points[0];
-					cp.Position = b1.GetWorldPoint(mp0.LocalPoint1);
-					Vec2 v1 = b1.GetLinearVelocityFromLocalPoint(mp0.LocalPoint1);
-					Vec2 v2 = b2.GetLinearVelocityFromLocalPoint(mp0.LocalPoint2);
-					cp.Velocity = v2 - v1;
-					cp.Normal = m0.Normal;
-					cp.Separation = mp0.Separation;
-					cp.ID = mp0.ID;
-					listener.Remove(cp);
-				}
-			}
-		}
-
-		new public static Contact Create(Shape shape1, Shape shape2)
-		{
-			return new CircleContact(shape1, shape2);
+			return new CircleContact(fixtureA, fixtureB);
 		}
 
 		new public static void Destroy(Contact contact)
