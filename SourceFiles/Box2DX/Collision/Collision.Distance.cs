064a457faf6da28fd3ac36dc53c1a83d828ba2d9
genbox_cp <SND\genbox_cp>
2009-06-29 02:13:55 +0000

    Adding hybrid solution to get rid of weird performance problem.

--- SourceFiles/Box2DX/Collision/Collision.Distance.cs
+++ SourceFiles/Box2DX/Collision/Collision.Distance.cs
@@ -475,13 +475,12 @@ namespace Box2DX.Collision
 
                 // Compute a tentative new simplex vertex using support points.
                 SimplexVertex vertex = vertices[simplex.Count];
-                vertex.IndexA = shapeA.GetSupport(Math.MulT(transformA.R, p));
-                vertex.WA = Math.Mul(transformA, shapeA.GetVertex(vertex.IndexA));
+                simplex.Vertices[simplex.Count].IndexA = shapeA.GetSupport(Math.MulT(transformA.R, p));
+                simplex.Vertices[simplex.Count].WA = Math.Mul(transformA, shapeA.GetVertex(vertex.IndexA));
                 Vec2 wBLocal;
-                vertex.IndexB = shapeB.GetSupport(Math.MulT(transformB.R, -p));
-                vertex.WB = Math.Mul(transformB, shapeB.GetVertex(vertex.IndexB));
-                vertex.W = vertex.WB - vertex.WA;
-                vertices[simplex.Count] = vertex;
+                simplex.Vertices[simplex.Count].IndexB = shapeB.GetSupport(Math.MulT(transformB.R, -p));
+                simplex.Vertices[simplex.Count].WB = Math.Mul(transformB, shapeB.GetVertex(vertex.IndexB));
+                simplex.Vertices[simplex.Count].W = vertex.WB - vertex.WA;
 
                 // Iteration count is equated to the number of support point calls.
                 ++iter;
