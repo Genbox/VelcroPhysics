d6e62048616514c771d19bce536ba56463246988
dinedal_cp <SND\dinedal_cp>
2009-07-08 23:32:28 +0000

    More inline with c++ version's behavior of modifying in place

--- SourceFiles/Box2DX/Collision/BroadPhase.cs
+++ SourceFiles/Box2DX/Collision/BroadPhase.cs
@@ -452,12 +452,12 @@ namespace Box2DX.Collision
                             }
 
                             ++_proxyPool[prevProxyId].UpperBounds[axis];
-                            ++bound.StabbingCount;
+                            ++bounds[index].StabbingCount;
                         }
                         else
                         {
                             ++_proxyPool[prevProxyId].LowerBounds[axis];
-                            --bound.StabbingCount;
+                            --bounds[index].StabbingCount;
                         }
 
                         --_proxyPool[proxyId].LowerBounds[axis];
@@ -486,12 +486,12 @@ namespace Box2DX.Collision
                             }
 
                             --_proxyPool[nextProxyId].LowerBounds[axis];
-                            ++bound.StabbingCount;
+                            ++bounds[index].StabbingCount;
                         }
                         else
                         {
                             --_proxyPool[nextProxyId].UpperBounds[axis];
-                            --bound.StabbingCount;
+                            --bounds[index].StabbingCount;
                         }
 
                         ++_proxyPool[proxyId].UpperBounds[axis];
@@ -525,12 +525,12 @@ namespace Box2DX.Collision
                             }
 
                             --_proxyPool[nextProxyId].UpperBounds[axis];
-                            --bound.StabbingCount;
+                            --bounds[index].StabbingCount;
                         }
                         else
                         {
                             --_proxyPool[nextProxyId].LowerBounds[axis];
-                            ++bound.StabbingCount;
+                            ++bounds[index].StabbingCount;
                         }
 
                         ++_proxyPool[proxyId].LowerBounds[axis];
@@ -560,12 +560,12 @@ namespace Box2DX.Collision
                             }
 
                             ++_proxyPool[prevProxyId].LowerBounds[axis];
-                            --bound.StabbingCount;
+                            --bounds[index].StabbingCount;
                         }
                         else
                         {
                             ++_proxyPool[prevProxyId].UpperBounds[axis];
-                            ++bound.StabbingCount;
+                            ++bounds[index].StabbingCount;
                         }
 
                         --_proxyPool[proxyId].UpperBounds[axis];
