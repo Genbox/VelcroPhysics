d8a95de731252fb86206cf286ffb848fe23c3c6e
Matthew Bettcher <mattbettcher@gmail.com>
2010-01-05 00:04:12 +0000

    Added Rendering Helpers to the DemoBaseXna project. Still not fully tested. These will become the backbone for the new brushes.

---
+++ Samples/FP3.0/DemoBaseXNA/DrawingSystem/LineRenderHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+//using DemoBaseXNA.DrawingSystem;
+using FarseerPhysics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DemoBaseXNA.DrawingSystem
+{
+
+    public class LineRenderHelper : BasePrimitiveRenderHelper
+    {
+        // Methods
+        public LineRenderHelper(int vertexcapacity)
+            : base(vertexcapacity)
+        {
+        }
+
+        public void Submit(Vector3 start, Vector3 end, Color color)
+        {
+            this.Submit(start, end, color, color);
+        }
+
+        public void Submit(Vector3 start, Vector3 end, Color startcolor, Color endcolor)
+        {
+            base.SubmitVertex(start, startcolor);
+            base.SubmitVertex(end, endcolor);
+        }
+
+        // Properties
+        public override int PrimitiveCount
+        {
+            get
+            {
+                return (base.VertexCount / 2);
+            }
+        }
+
+        protected override PrimitiveType PrimitiveType
+        {
+            get
+            {
+                return PrimitiveType.LineList;
+            }
+        }
+    }
+}
+
