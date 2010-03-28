cad8335aa199a71668f0203f34e8b9231390f46e
Ian Qvist <Genbox@users.noreply.github.com>
2010-03-28 15:30:05 +0000

    Added CreateRoundedRectangle() and CreateCapsule() to PolygonTools.cs

--- SourceFiles/FP3.0/Common/PolygonTools.cs
+++ SourceFiles/FP3.0/Common/PolygonTools.cs
@@ -47,6 +47,68 @@ namespace FarseerPhysics
             return vertices;
         }
 
+        //Contributed by Jonathan Smars
+        /// <summary>
+        /// Creates a rounded rectangle with the specified width and height.
+        /// </summary>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        /// <param name="xRadius">The rounding X radius.</param>
+        /// <param name="yRadius">The rounding Y radius.</param>
+        /// <param name="segments">The number of segments to subdivide the edges.</param>
+        /// <returns></returns>
+        public static Vertices CreateRoundedRectangle(float width, float height, float xRadius, float yRadius, float segments)
+        {
+            if (yRadius > height / 2 || xRadius > width / 2)
+                throw new Exception("Rounding amount can't be more than half the height and width respectively.");
+            if (segments < 0)
+                throw new Exception("Segments must be zero or above.");
+
+            Vertices vertices = new Vertices();
+            if (segments == 0)
+            {
+                vertices.Add(new Vector2(-width * .5f + xRadius, -height * .5f));
+                vertices.Add(new Vector2(-width * .5f, -height * .5f + yRadius));
+
+                vertices.Add(new Vector2(-width * .5f, height * .5f - yRadius));
+                vertices.Add(new Vector2(-width * .5f + xRadius, height * .5f));
+
+                vertices.Add(new Vector2(width * .5f - xRadius, height * .5f));
+                vertices.Add(new Vector2(width * .5f, height * .5f - yRadius));
+
+                vertices.Add(new Vector2(width * .5f, -height * .5f + yRadius));
+                vertices.Add(new Vector2(width * .5f - xRadius, -height * .5f));
+            }
+            else
+            {
+                float numberOfEdges = (segments * 4 + 8);
+                float stepSize = MathHelper.TwoPi / (numberOfEdges - 4);
+                int perPhase = (int)numberOfEdges / 4;
+
+                Vector2 posOffset = new Vector2(width / 2 - xRadius, -height / 2 + yRadius);
+                vertices.Add(posOffset + new Vector2(xRadius, -yRadius + yRadius));
+                short phase = 0;
+                for (int i = 1; i < numberOfEdges; i++)
+                {
+                    if (i - perPhase == 0 || i - perPhase * 3 == 0)
+                    {
+                        posOffset.X *= -1;
+                        phase--;
+                    }
+                    else if (i - perPhase * 2 == 0)
+                    {
+                        posOffset.Y *= -1;
+                        phase--;
+                    }
+
+                    vertices.Add(posOffset + new Vector2(xRadius * (float)Math.Cos(stepSize * (i + phase)),
+                                                         -yRadius * (float)Math.Sin(stepSize * (i + phase))));
+                }
+            }
+
+            return vertices;
+        }
+        
         /// <summary>
         /// Set this as a single edge.
         /// </summary>
@@ -88,6 +150,85 @@ namespace FarseerPhysics
             vertices.Add(new Vector2(xRadius, 0));
             for (int i = 1; i < numberOfEdges; i++)
                 vertices.Add(new Vector2(xRadius * (float)Math.Cos(stepSize * i), -yRadius * (float)Math.Sin(stepSize * i)));
+
+            return vertices;
+        }
+
+        //Contributed by Yobiv
+        /// <summary>
+        /// Creates an capsule with the specified height, radius and number of edges.
+        /// A capsule has the same form as a pill capsule.
+        /// </summary>
+        /// <param name="height">Height (inner height + 2 * radius) of the capsule.</param>
+        /// <param name="endRadius">Radius of the capsule ends.</param>
+        /// <param name="edges">The number of edges of the capsule ends. The more edges, the more it resembles an capsule</param>
+        /// <returns></returns>
+        public static Vertices CreateCapsule(float height, float endRadius, int edges)
+        {
+            if (endRadius >= height / 2)
+                throw new ArgumentException("The radius must be lower than height / 2. Higher values of radius would create a circle, and not a half circle.", "endRadius");
+
+            return CreateCapsule(height, endRadius, edges, endRadius, edges);
+        }
+
+        /// <summary>
+        /// Creates an capsule with the specified  height, radius and number of edges.
+        /// A capsule has the same form as a pill capsule.
+        /// </summary>
+        /// <param name="height">Height (inner height + radii) of the capsule.</param>
+        /// <param name="topRadius">Radius of the top.</param>
+        /// <param name="topEdges">The number of edges of the top. The more edges, the more it resembles an capsule</param>
+        /// <param name="bottomRadius">Radius of bottom.</param>
+        /// <param name="bottomEdges">The number of edges of the bottom. The more edges, the more it resembles an capsule</param>
+        /// <returns></returns>
+        public static Vertices CreateCapsule(float height, float topRadius, int topEdges, float bottomRadius, int bottomEdges)
+        {
+            if (height <= 0)
+                throw new ArgumentException("Height must be longer than 0", "height");
+
+            if (topRadius <= 0)
+                throw new ArgumentException("The top radius must be more than 0", "topRadius");
+
+            if (topEdges <= 0)
+                throw new ArgumentException("Top edges must be more than 0", "topEdges");
+
+            if (bottomRadius <= 0)
+                throw new ArgumentException("The bottom radius must be more than 0", "bottomRadius");
+
+            if (bottomEdges <= 0)
+                throw new ArgumentException("Bottom edges must be more than 0", "bottomEdges");
+
+            if (topRadius >= height / 2)
+                throw new ArgumentException("The top radius must be lower than height / 2. Higher values of top radius would create a circle, and not a half circle.", "topRadius");
+
+            if (bottomRadius >= height / 2)
+                throw new ArgumentException("The bottom radius must be lower than height / 2. Higher values of bottom radius would create a circle, and not a half circle.", "bottomRadius");
+
+            Vertices vertices = new Vertices();
+
+            float newHeight = (height - topRadius - bottomRadius) * 0.5f;
+
+            // top
+            vertices.Add(new Vector2(topRadius, newHeight));
+
+            float stepSize = MathHelper.Pi / topEdges;
+            for (int i = 1; i < topEdges; i++)
+            {
+                vertices.Add(new Vector2(topRadius * (float)Math.Cos(stepSize * i), topRadius * (float)Math.Sin(stepSize * i) + newHeight));
+            }
+
+            vertices.Add(new Vector2(-topRadius, newHeight));
+
+            // bottom
+            vertices.Add(new Vector2(-bottomRadius, -newHeight));
+
+            stepSize = MathHelper.Pi / bottomEdges;
+            for (int i = 1; i < bottomEdges; i++)
+            {
+                vertices.Add(new Vector2(-bottomRadius * (float)Math.Cos(stepSize * i), -bottomRadius * (float)Math.Sin(stepSize * i) - newHeight));
+            }
+
+            vertices.Add(new Vector2(bottomRadius, -newHeight));
 
             return vertices;
         }
