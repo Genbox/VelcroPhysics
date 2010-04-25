2ae9c53d6be4079c5ab443e1e49f19e23698fd76
Matthew Bettcher <mattbettcher@gmail.com>
2010-04-25 19:22:02 +0000

    Added JointFactory. (unfinished) Update PathTest. Minor change to PathFactory.

---
+++ Branches/FP3.0/XNA/Factories/JointFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using FarseerPhysics.Collision;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Common;
+using FarseerPhysics.Common.Decomposition;
+using FarseerPhysics.Common.PolygonManipulation;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Dynamics.Joints;
+using Microsoft.Xna.Framework;
+
+namespace FarseerPhysics.Factories
+{
+    /// <summary>
+    /// An easy to use factory for using joints.
+    /// </summary>
+    public static class JointFactory
+    {
+        
+        public static void AttachBodiesWithRevoluteJoint(World world, List<Body> bodies, Vector2 localAnchorA, Vector2 localAnchorB, bool connectFirstAndLast, bool collideConnected)
+        {
+            for (int i = 1; i < bodies.Count; i++)
+            {
+                RevoluteJoint joint = new RevoluteJoint(bodies[i], bodies[i - 1], new Vector2());
+                joint.LocalAnchorA = localAnchorA;
+                joint.LocalAnchorB = localAnchorB;
+                joint.CollideConnected = collideConnected;
+                world.Add(joint);
+            }
+
+            if (connectFirstAndLast)
+            {
+                RevoluteJoint lastjoint = new RevoluteJoint(bodies[0], bodies[bodies.Count - 1], new Vector2());
+                lastjoint.LocalAnchorA = localAnchorA;
+                lastjoint.LocalAnchorB = localAnchorB;
+                lastjoint.CollideConnected = collideConnected;
+                world.Add(lastjoint);
+            }
+        }
+
+    }
+}
