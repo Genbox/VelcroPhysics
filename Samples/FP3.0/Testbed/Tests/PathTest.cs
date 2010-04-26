d67e860a309e0b8e2f187b1066e5431777ad5d2a
mattbettcher_cp <SND\mattbettcher_cp>
2010-04-26 00:44:30 +0000

    Added more features to PathFactory. Updated PathTest.

--- Samples/FP3.0/Testbed/Tests/PathTest.cs
+++ Samples/FP3.0/Testbed/Tests/PathTest.cs
@@ -12,6 +12,9 @@ namespace FarseerPhysics.TestBed.Tests
 {
     public class PathTest : Test
     {
+        Body movingBody;
+        Path path;
+
         private PathTest()
         {
             Body ground;
@@ -21,10 +24,16 @@ namespace FarseerPhysics.TestBed.Tests
                 Vertices edge = PolygonTools.CreateEdge(new Vector2(-40.0f, 0.0f), new Vector2(40.0f, 0.0f));
                 PolygonShape shape = new PolygonShape(edge, 0);
                 ground.CreateFixture(shape);
+
+                movingBody = BodyFactory.CreateBody(World);
+                movingBody.Position = new Vector2(-25, 25);
+                movingBody.BodyType = BodyType.Dynamic;
+                movingBody.CreateFixture(new PolygonShape(PolygonTools.CreateRectangle(0.5f, 0.5f), 1));
+                World.Add(movingBody);
             }
 
             {
-                Path path = new Path();
+                path = new Path();
 
                 path.Add(new Vector2(0, 20));
                 path.Add(new Vector2(5, 15));
@@ -39,7 +48,7 @@ namespace FarseerPhysics.TestBed.Tests
                 //body.CreateFixture(new PolygonShape(PolygonTools.CreateRectangle(0.5f, 0.5f, new Vector2(-0.5f, 0), 0), 1));
                 Fixture f = body.CreateFixture(new CircleShape(0.25f, 1));
 
-                PathFactory.EvenlyDistibuteShapesAlongPath(World, path, body, 10);
+                PathFactory.EvenlyDistibuteShapesAlongPath(World, path, body, 100);
 
                 Vector2 xform = new Vector2(0.5f, 0.5f);
                 path.Scale(ref xform);
@@ -66,7 +75,32 @@ namespace FarseerPhysics.TestBed.Tests
                 PathFactory.ConvertPathToEdges(World, path, body, 25);
 
                 World.Add(body);
+
+                xform = new Vector2(0, 25);
+                path.Translate(ref xform);
+
+                body = new Body(World);
+                body.BodyType = BodyType.Dynamic;
+
+                path.Closed = true;
+
+                PathFactory.ConvertPathToPolygon(World, path, body, 1, 50);
+                body.Position -= new Vector2(0, 10);
+                World.Add(body);
             }
+        }
+
+        float time = 0;
+
+        public override void Update(GameSettings settings, GameTime gameTime)
+        {
+            time += 0.01f;
+            if (time > 1f)
+                time = 0;
+            
+            PathFactory.MoveBodyOnPath(path, movingBody, time, 1f, 1f / 60f);
+            
+            base.Update(settings, gameTime);
         }
 
         internal static Test Create()
