5011ec1c7f11db9ed111ed6a22cc45df7ca32ad5
Ian Qvist <Genbox@users.noreply.github.com>
2010-08-16 22:52:39 +0000

    Deleted Demo10 in SimpleSampels Updated Agent to support collision categories Added Objects class that can create strips of circles, rectangles, stars and gears Created Demo4 and Demo5

--- Samples/FP3.0/SimpleSamplesXNA/Demo5/Demo5Screen.cs
+++ Samples/FP3.0/SimpleSamplesXNA/Demo5/Demo5Screen.cs
@@ -1,10 +1,70 @@
 ﻿using System.Text;
+using FarseerPhysics.DemoBaseXNA.DemoShare;
 using FarseerPhysics.DemoBaseXNA.ScreenSystem;
+using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
 
 namespace SimpleSamplesXNA.Demo5
 {
     internal class Demo5Screen : GameScreen
     {
+        private Agent _agent;
+        private Objects _circles;
+        private Objects _rectangles;
+        private Objects _gears;
+        private Objects _stars;
+
+        public override void Initialize()
+        {
+            World = new World(Vector2.Zero);
+
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            //Cat1=Circles, Cat2=Rectangles, Cat3=Gears, Cat4=Stars, Cat5=Agent
+            _agent = new Agent(World, Vector2.Zero);
+            _agent.CollisionCategories = CollisionCategory.Cat5;
+
+            //Collide with all but cat4
+            _agent.CollidesWith = CollisionCategory.All & ~CollisionCategory.Cat4;
+
+            Vector2 startPosition = new Vector2(-20, 17);
+            Vector2 endPosition = new Vector2(20, 17);
+            _circles = new Objects(World, startPosition, endPosition, 15, 1, ObjectType.Circle);
+            _circles.CollisionCategories = (CollisionCategory.Cat1);
+
+            //Collide with stars and gears
+            _circles.CollidesWith = (CollisionCategory.Cat3 | CollisionCategory.Cat4);
+
+            startPosition = new Vector2(-20, -17);
+            endPosition = new Vector2(20, -17);
+            _rectangles = new Objects(World, startPosition, endPosition, 15, 2, ObjectType.Rectangle);
+            _rectangles.CollisionCategories = (CollisionCategory.Cat2);
+
+            //Collides with rectangles (itself only)
+            _rectangles.CollidesWith = CollisionCategory.Cat2;
+
+            startPosition = new Vector2(-20, -10);
+            endPosition = new Vector2(-20, 10);
+            _gears = new Objects(World, startPosition, endPosition, 5, 1, ObjectType.Gear);
+            _gears.CollisionCategories = (CollisionCategory.Cat3);
+
+            //Collides with itself (gears) and stars
+            _gears.CollidesWith = (CollisionCategory.Cat3 | CollisionCategory.Cat4);
+
+            startPosition = new Vector2(20, -10);
+            endPosition = new Vector2(20, 10);
+            _stars = new Objects(World, startPosition, endPosition, 5, 1, ObjectType.Star);
+            _stars.CollisionCategories = (CollisionCategory.Cat4);
+
+            //Collides with gears and itself (stars)
+            _stars.CollidesWith = (CollisionCategory.Cat3 | CollisionCategory.Cat4);
+
+            base.LoadContent();
+        }
+
         public string GetTitle()
         {
             return "Demo5: Collision Categories";
