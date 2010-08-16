441a982da64dd702808bc0f01373b26bee16c208
genbox_cp <SND\genbox_cp>
2010-08-16 14:07:14 +0000

    Created Agent in DemoBaseXNA Created Demo3 in SimpleSamples Extended FixtureFactory to take in an already existing body in CreateRectangle() and CreateCircle(). This allows you to attach the fixture you just created to an already existing body.

--- Samples/FP3.0/SimpleSamplesXNA/Demo3/Demo3Screen.cs
+++ Samples/FP3.0/SimpleSamplesXNA/Demo3/Demo3Screen.cs
@@ -1,28 +1,26 @@
 ﻿using System.Text;
+using FarseerPhysics.DemoBaseXNA.DemoShare;
 using FarseerPhysics.DemoBaseXNA.ScreenSystem;
 using FarseerPhysics.Dynamics;
 using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
 
 namespace SimpleSamplesXNA.Demo3
 {
     internal class Demo3Screen : GameScreen
     {
-        //private Agent _agent;
         private Fixture[] _obstacles = new Fixture[5];
 
         public override void Initialize()
         {
-            World = new World(new Vector2(0, -200));
+            World = new World(new Vector2(0, -20));
 
             base.Initialize();
         }
 
         public override void LoadContent()
         {
-            //_agent = new Agent(new Vector2(ScreenManager.ScreenCenter.X, 100));
-            //_agent.Load(ScreenManager.GraphicsDevice, World);
+            new Agent(World, new Vector2(5, 10));
 
             LoadObstacles();
 
@@ -33,7 +31,7 @@ namespace SimpleSamplesXNA.Demo3
         {
             for (int i = 0; i < 5; i++)
             {
-                _obstacles[i] = FixtureFactory.CreateRectangle(World, 10, 2, 1);
+                _obstacles[i] = FixtureFactory.CreateRectangle(World, 8, 1.5f, 1);
                 _obstacles[i].Body.IsStatic = true;
 
                 if (i == 0)
@@ -43,69 +41,11 @@ namespace SimpleSamplesXNA.Demo3
                 }
             }
 
-            _obstacles[0].Body.Position = ScreenManager.ScreenCenter + new Vector2(-5, -20);
-            _obstacles[1].Body.Position = ScreenManager.ScreenCenter + new Vector2(15, -10);
-            _obstacles[2].Body.Position = ScreenManager.ScreenCenter + new Vector2(10, 5);
-            _obstacles[3].Body.Position = ScreenManager.ScreenCenter + new Vector2(-10, 20);
-            _obstacles[4].Body.Position = ScreenManager.ScreenCenter + new Vector2(-17, 0);
-        }
-
-        public override void HandleInput(InputState input)
-        {
-            if (firstRun)
-            {
-                ScreenManager.AddScreen(new PauseScreen(GetTitle(), GetDetails()));
-                firstRun = false;
-            }
-            if (input.PauseGame)
-            {
-                ScreenManager.AddScreen(new PauseScreen(GetTitle(), GetDetails()));
-            }
-
-            if (input.CurrentGamePadState.IsConnected)
-            {
-                HandleGamePadInput(input);
-            }
-            else
-            {
-                HandleKeyboardInput(input);
-            }
-            base.HandleInput(input);
-        }
-
-        private void HandleGamePadInput(InputState input)
-        {
-            Vector2 force = 800 * input.CurrentGamePadState.ThumbSticks.Left;
-            force.Y = -force.Y;
-            //_agent.Body.ApplyForce(force);
-
-            float rotation = -8000 * input.CurrentGamePadState.Triggers.Left;
-            //_agent.Body.ApplyTorque(rotation);
-
-            rotation = 8000 * input.CurrentGamePadState.Triggers.Right;
-            //_agent.Body.ApplyTorque(rotation);
-        }
-
-        private void HandleKeyboardInput(InputState input)
-        {
-            const float forceAmount = 800;
-            Vector2 force = Vector2.Zero;
-            force.Y = -force.Y;
-
-            if (input.CurrentKeyboardState.IsKeyDown(Keys.A)) { force += new Vector2(-forceAmount, 0); }
-            if (input.CurrentKeyboardState.IsKeyDown(Keys.S)) { force += new Vector2(0, forceAmount); }
-            if (input.CurrentKeyboardState.IsKeyDown(Keys.D)) { force += new Vector2(forceAmount, 0); }
-            if (input.CurrentKeyboardState.IsKeyDown(Keys.W)) { force += new Vector2(0, -forceAmount); }
-
-            //_agent.Body.ApplyForce(force);
-
-            const float torqueAmount = 8000;
-            float torque = 0;
-
-            if (input.CurrentKeyboardState.IsKeyDown(Keys.Left)) { torque -= torqueAmount; }
-            if (input.CurrentKeyboardState.IsKeyDown(Keys.Right)) { torque += torqueAmount; }
-
-            //_agent.Body.ApplyTorque(torque);
+            _obstacles[0].Body.Position = new Vector2(-5, -15);
+            _obstacles[1].Body.Position = new Vector2(15, -10);
+            _obstacles[2].Body.Position = new Vector2(10, 5);
+            _obstacles[3].Body.Position = new Vector2(-10, 15);
+            _obstacles[4].Body.Position = new Vector2(-17, 0);
         }
 
         public string GetTitle()
