2086ed0d53f64893e130ec1fa8d9994a3459441f
TheNeo_cp <SND\TheNeo_cp>
2010-08-18 18:09:40 +0000

    Added ragdoll object along with four obstacles.

--- Samples/FP3.0/SimpleSamplesXNA/Demo9/Demo9Screen.cs
+++ Samples/FP3.0/SimpleSamplesXNA/Demo9/Demo9Screen.cs
@@ -1,16 +1,26 @@
 ﻿using System.Text;
+using FarseerPhysics.DemoBaseXNA.DemoShare;
+using FarseerPhysics.DemoBaseXNA;
 using FarseerPhysics.DemoBaseXNA.ScreenSystem;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace SimpleSamplesXNA.Demo9
 {
-    internal class Demo9Screen : GameScreen
+
+    
+    internal class Demo9Screen : GameScreen, IDemoScreen
     {
+        #region IDemoScreen Members
+
         public string GetTitle()
         {
             return "Demo9: Ragdoll";
         }
 
-        private string GetDetails()
+        public string GetDetails()
         {
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("This demo shows how to combine physics objects");
@@ -19,6 +29,39 @@ namespace SimpleSamplesXNA.Demo9
             sb.AppendLine("Mouse");
             sb.AppendLine("  -Hold down left button and drag");
             return sb.ToString();
+        }
+
+        #endregion 
+
+        Ragdoll _ragdoll;
+
+        public override void Initialize()
+        {
+            World = new World(new Vector2(0,0));
+            World.Gravity = new Vector2(0, -20);
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            _ragdoll = new Ragdoll(World,new Vector2(0,0));
+            CreateObstacles();
+            base.LoadContent();
+        }
+
+        private void CreateObstacles()
+        {
+            Fixture[] rect = new Fixture[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                rect[i] = FixtureFactory.CreateRectangle(World, 6, 1.5f, 1);
+            }
+            rect[0].Body.Position = new Vector2(-9, -5);
+            rect[1].Body.Position = new Vector2(-8, 7);
+            rect[2].Body.Position = new Vector2(9, -7);
+            rect[3].Body.Position = new Vector2(7, 5);
+            
         }
     }
 }
