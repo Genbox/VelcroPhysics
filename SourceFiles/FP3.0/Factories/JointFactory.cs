31d4fc0de9a8d162bafcc274956811c6caf827c1
RobertDodd_cp <SND\RobertDodd_cp>
2010-08-18 06:59:37 +0000

    Added simple RevoluteJoint and WeldJoint constructors to the JointFactory

--- SourceFiles/FP3.0/Factories/JointFactory.cs
+++ SourceFiles/FP3.0/Factories/JointFactory.cs
@@ -1,9 +1,78 @@
-﻿namespace FarseerPhysics.Factories
+﻿using FarseerPhysics.Dynamics;
+using FarseerPhysics.Dynamics.Joints;
+
+using Microsoft.Xna.Framework;
+
+namespace FarseerPhysics.Factories
 {
     /// <summary>
     /// An easy to use factory for using joints.
     /// </summary>
     public static class JointFactory
     {
+        #region Revolute Joint
+        /// <summary>
+        /// Creates a revolute joint and adds it to the world
+        /// </summary>
+        /// <param name="world"></param>
+        /// <param name="bodyA"></param>
+        /// <param name="bodyB"></param>
+        /// <param name="localanchorA"></param>
+        /// <returns></returns>
+        public static RevoluteJoint CreateRevoluteJoint(Body bodyA, Body bodyB, Vector2 localanchorA)
+        {
+            Vector2 localanchorB = bodyB.GetLocalPoint(bodyA.GetWorldPoint(localanchorA));
+            RevoluteJoint joint = new RevoluteJoint(bodyA, bodyB, localanchorA, localanchorB);
+            return joint;
+        }
+
+        /// <summary>
+        /// Creates a revolute joint and adds it to the world
+        /// </summary>
+        /// <param name="world"></param>
+        /// <param name="bodyA"></param>
+        /// <param name="bodyB"></param>
+        /// <param name="localanchorA"></param>
+        /// <returns></returns>
+        public static RevoluteJoint CreateRevoluteJoint(World world, Body bodyA, Body bodyB, Vector2 localanchorA)
+        {
+            RevoluteJoint joint = CreateRevoluteJoint(bodyA, bodyB, localanchorA);
+            world.AddJoint(joint);
+            return joint;
+        }
+        #endregion
+
+        #region Weld Joint
+        /// <summary>
+        /// Creates a weld joint
+        /// </summary>
+        /// <param name="world"></param>
+        /// <param name="bodyA"></param>
+        /// <param name="bodyB"></param>
+        /// <param name="localanchorA"></param>
+        /// <returns></returns>
+        public static WeldJoint CreateWeldJoint(Body bodyA, Body bodyB, Vector2 localanchorA)
+        {
+            Vector2 localanchorB = bodyB.GetLocalPoint(bodyA.GetWorldPoint(localanchorA));
+            WeldJoint joint = new WeldJoint(bodyA, bodyB, localanchorA, localanchorB);
+            return joint;
+        }
+
+        /// <summary>
+        /// Creates a weld joint and adds it to the world
+        /// </summary>
+        /// <param name="world"></param>
+        /// <param name="bodyA"></param>
+        /// <param name="bodyB"></param>
+        /// <param name="localanchorA"></param>
+        /// <returns></returns>
+        public static WeldJoint CreateWeldJoint(World world, Body bodyA, Body bodyB, Vector2 localanchorA)
+        {
+            WeldJoint joint = CreateWeldJoint(bodyA, bodyB, localanchorA);
+            world.AddJoint(joint);
+            return joint;
+        }
+        #endregion
+        
     }
 }
