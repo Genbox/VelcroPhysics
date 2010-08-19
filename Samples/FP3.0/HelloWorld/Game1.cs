d7fc06be4dc4c7486c5d4f19d7e917d4faa415c3
genbox_cp <SND\genbox_cp>
2010-08-19 22:19:43 +0000

    Added Hello World sample. It uses DebugViewXNA to draw the screen elements.

---
+++ Samples/FP3.0/HelloWorld/Game1.cs
@@ -0,0 +1,94 @@
+using FarseerPhysics.DebugViewXNA;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+
+        //The most important object of them all; the world.
+        private World _world = new World(new Vector2(0, -20));
+
+        private Fixture _rectangleFixture;
+        private Fixture _circleFixture;
+        
+        private DebugViewXNA _debugView;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            _debugView = new DebugViewXNA(_world);
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            DebugViewXNA.LoadContent(_graphics.GraphicsDevice, Content);
+
+            _rectangleFixture = FixtureFactory.CreateRectangle(_world, 50, 5, 1, new Vector2(0, 0));
+            _rectangleFixture.Body.IsStatic = true;
+
+            //Give it some bounce and friction
+            _rectangleFixture.Restitution = 0.3f;
+            _rectangleFixture.Friction = 0.5f;
+
+            //Create the circle fixture
+            _circleFixture = FixtureFactory.CreateCircle(_world, 2, 1, new Vector2(10, 10));
+            _circleFixture.Body.BodyType = BodyType.Dynamic;
+
+            _circleFixture.Restitution = 0.3f;
+            _circleFixture.Friction = 0.5f;
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState s = Keyboard.GetState();
+
+            //We make it possible to rotate the circle body
+            if (s.IsKeyDown(Keys.A))
+                _circleFixture.Body.ApplyTorque(100);
+
+            if (s.IsKeyDown(Keys.D))
+                _circleFixture.Body.ApplyTorque(-100);
+
+            //We update the world
+            _world.Step((float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            Matrix proj = Matrix.CreateOrthographic(50 * _graphics.GraphicsDevice.Viewport.AspectRatio, 50, 0, 1);
+            Matrix view = Matrix.Identity;
+
+            _debugView.DrawDebugData();
+            _debugView.RenderDebugData(ref proj, ref view);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Samples/FP3.0/HelloWorld/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hello World XNA Demo")]
+[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Farseer Physics Engine")]
+
+[assembly: AssemblyCopyright("Copyright © Farseer Physics 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eb54a000-ca03-4bf5-9907-9e0b14af9e81")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.0.*")]
