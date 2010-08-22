ef6927eb51991bc75cf4f77c65634af7db270b9f
genbox_cp <SND\genbox_cp>
2010-08-22 19:41:54 +0000

    Updated AssemblyInfo in class library and silverlight library.

--- Branches/FP3.0/Class/Properties/AssemblyInfo.cs
+++ Branches/FP3.0/Class/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Farseer Physics 3.0")]
+[assembly: AssemblyTitle("Farseer Physics Engine 3.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Farseer Physics 3.0")]
-[assembly: AssemblyCopyright("Copyright © 2009")]
+[assembly: AssemblyProduct("Farseer Physics Engine 3.0")]
+[assembly: AssemblyCopyright("Copyright © Farseer Physics 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Branches/FP3.0/Silverlight/Farseer Physics 3.0 Silverlight/Properties/AssemblyInfo.cs
+++ Branches/FP3.0/Silverlight/Farseer Physics 3.0 Silverlight/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Farseer_Physics_3._0_Silverlight")]
+[assembly: AssemblyTitle("Farseer_Physics 3.0 Silverlight")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Farseer_Physics_3._0_Silverlight")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Farseer Physics 3.0 Silverlight")]
+[assembly: AssemblyCopyright("Copyright © Farseer Physics 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.*")]
+[assembly: AssemblyFileVersion("3.0.*")]
