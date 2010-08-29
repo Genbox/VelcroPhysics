e35825dcd1ffaffe9513cf1b9b5e27ce708a6e91
Ian Qvist <Genbox@users.noreply.github.com>
2010-08-29 15:20:55 +0000

    Deleting FP3.0 folders Renamed projects (exclude version number) Does not build!

---
+++ Samples/Testbed/FrameRateComponet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FarseerPhysics.Components
+{
+    /// <summary>
+    /// Displays the FPS
+    /// </summary>
+    public class FrameRateCounter : DrawableGameComponent
+    {
+        private ContentManager _content;
+        private TimeSpan _elapsedTime = TimeSpan.Zero;
+        private SpriteFont _font;
+        private NumberFormatInfo _format;
+        private int _frameCounter;
+        private int _frameRate;
+        private SpriteBatch _sb;
+
+        public FrameRateCounter(Game game, GraphicsDevice device, ContentManager content)
+            : base(game)
+        {
+            _sb = new SpriteBatch(device);
+            _content = content;
+            _format = new NumberFormatInfo();
+            _format.NumberDecimalSeparator = ".";
+        }
+
+        protected override void LoadContent()
+        {
+            _font = _content.Load<SpriteFont>("Monaco");
+
+            base.LoadContent();
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            _elapsedTime += gameTime.ElapsedGameTime;
+
+            if (_elapsedTime <= TimeSpan.FromSeconds(1)) return;
+
+            _elapsedTime -= TimeSpan.FromSeconds(1);
+            _frameRate = _frameCounter;
+            _frameCounter = 0;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _frameCounter++;
+
+            string fps = string.Format(_format, "fps: {0}", _frameRate);
+
+            _sb.Begin();
+            _sb.DrawString(_font, fps, new Vector2(100, 80), Color.Black);
+            _sb.End();
+        }
+    }
+}
---
+++ Samples/Testbed/Tests/LineJoin.cs
@@ -0,0 +1,74 @@
+/*
+* Box2D.XNA port of Box2D:
+* Copyright (c) 2009 Brandon Furtwangler, Nathan Furtwangler
+*
+* Original source Box2D:
+* Copyright (c) 2006-2009 Erin Catto http://www.gphysics.com 
+* 
+* This software is provided 'as-is', without any express or implied 
+* warranty.  In no event will the authors be held liable for any damages 
+* arising from the use of this software. 
+* Permission is granted to anyone to use this software for any purpose, 
+* including commercial applications, and to alter it and redistribute it 
+* freely, subject to the following restrictions: 
+* 1. The origin of this software must not be misrepresented; you must not 
+* claim that you wrote the original software. If you use this software 
+* in a product, an acknowledgment in the product documentation would be 
+* appreciated but is not required. 
+* 2. Altered source versions must be plainly marked as such, and must not be 
+* misrepresented as being the original software. 
+* 3. This notice may not be removed or altered from any source distribution. 
+*/
+
+using FarseerPhysics.TestBed.Framework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FarseerPhysics.TestBed.Tests
+{
+    public class LineJointTest : Test
+    {
+        private LineJointTest()
+        {
+            Body ground;
+            {
+                PolygonShape shape = new PolygonShape(PolygonTools.CreateEdge(new Vector2(-40.0f, 0.0f), new Vector2(40.0f, 0.0f)), 0);
+
+                
+                ground = World.CreateBody();
+                ground.CreateFixture(shape);
+            }
+
+            {
+                PolygonShape shape = new PolygonShape(PolygonTools.CreateRectangle(0.5f, 2.0f), 1);
+
+                Body body = World.CreateBody();
+                body.BodyType = BodyType.Dynamic;
+                body.Position = new Vector2(0.0f, 7.0f);
+
+                body.CreateFixture(shape);
+
+                Vector2 axis = new Vector2(2.0f, 1.0f);
+                axis.Normalize();
+                //LineJoint jd = new LineJoint(ground, body, new Vector2(0.0f, 8.5f),new Vector2(1.0f, 2.5f), axis);
+                FixedLineJoint jd = new FixedLineJoint(body, /*ground, */new Vector2(1.0f, 1.5f),new Vector2(10.0f, 8.5f), axis);
+                jd.MotorSpeed = 100.0f;
+                jd.MaxMotorForce = 100.0f;
+                jd.MotorEnabled = false;
+                jd.LowerLimit = -4.0f;
+                jd.UpperLimit = 4.0f;
+                jd.EnableLimit = true;
+                World.CreateJoint(jd);
+                _jd = jd;
+            }
+        }
+
+        internal static Test Create()
+        {
+            return new LineJointTest();
+        }
+
+        FixedLineJoint _jd;
+        //LineJoint _jd;
+    }
+}
