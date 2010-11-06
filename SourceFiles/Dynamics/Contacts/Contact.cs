6823488957a054dd02ed520028e2af4cbf3584c4
genbox_cp <SND\genbox_cp>
2010-11-06 23:48:01 +0000

    Changed OnCollision strategy. - If the first subscription to OnCollision returns false, the second subscription will not fire. - This behavior is identical to FPE 2.1.3

--- SourceFiles/Dynamics/Contacts/Contact.cs
+++ SourceFiles/Dynamics/Contacts/Contact.cs
@@ -367,7 +367,7 @@ namespace FarseerPhysics.Dynamics.Contacts
 
                 //Reverse the order of the reported fixtures. The first fixture is always the one that the
                 //user subscribed to.
-                if (FixtureB.OnCollision != null)
+                if (Enabled && FixtureB.OnCollision != null)
                     Enabled = FixtureB.OnCollision(FixtureB, FixtureA, this);
 
                 //if the user disabled the contact (needed to exclude it in TOI solver), we also need to mark
