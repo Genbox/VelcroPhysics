a9777cea80e0a09783e0efbb7b8bea82d508d210
instinctvfx_cp <SND\instinctvfx_cp>
2010-11-27 19:35:11 +0000

    Added SimpleWindForce. Beware, this is currently used for testing and NOT an actual implementation of SimpleWindForce

---
+++ SourceFiles/Controllers/SimpleWindForce.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+using FarseerPhysics.Common;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Controllers;
+
+namespace FarseerPhysics.Controllers
+{
+    public class SimpleWindForce:AbstractForceController
+    {
+        //public PerlinNoise Noise;
+        public Noise Noise;
+
+        Random Randomizer;
+
+        public SimpleWindForce(): base()
+        {
+            Randomizer = new Random(12345);
+        }
+
+        public override void ApplyForce(float dt, float Strength)
+        {
+            foreach (Body body in this.World.BodyList)
+            {
+                Vector2 forceVector = body.Position - this.Position;
+                float distance = forceVector.Length();
+                forceVector.Normalize();
+
+                body.ApplyForce(forceVector * Strength * (10 / distance) * (float)Randomizer.NextDouble());
+            }
+        }
+
+    }
+}
