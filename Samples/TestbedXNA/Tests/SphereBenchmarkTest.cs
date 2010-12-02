81a4f4cc627d9aef18dbb4297ac31ad3c0577d5c
Ian Qvist <Genbox@users.noreply.github.com>
2010-12-02 03:15:45 +0000

    Added SphereBenchmarkTest - Used to test performance of circle vs circle collisions DebugViewXNA now uses 16 segments (instead of 32) to draw circles on Xbox

---
+++ Samples/TestbedXNA/Tests/SphereBenchmarkTest.cs
@@ -0,0 +1,83 @@
+﻿/*
+* Farseer Physics Engine based on Box2D.XNA port:
+* Copyright (c) 2010 Ian Qvist
+* 
+* Box2D.XNA port of Box2D:
+* Copyright (c) 2009 Brandon Furtwangler, Nathan Furtwangler
+*
+* Original source Box2D:
+* Copyright (c) 2006-2009 Erin Catto http://www.gphysics.com 
+* 
+* This software is provided 'as-is', without any express or implied 
+* warranty.  In no event will the authors be held liable for any damages 
+* arising from the use of this software. 
+* Permission is granted to anyone to use this software for any purpose, 
+* including commercial applications, and to alter it and redistribute it 
+* freely, subject to the following restrictions: 
+* 1. The origin of this software must not be misrepresented; you must not 
+* claim that you wrote the original software. If you use this software 
+* in a product, an acknowledgment in the product documentation would be 
+* appreciated but is not required. 
+* 2. Altered source versions must be plainly marked as such, and must not be 
+* misrepresented as being the original software. 
+* 3. This notice may not be removed or altered from any source distribution. 
+*/
+
+using System.Collections.Generic;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Common;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using FarseerPhysics.TestBed.Framework;
+using Microsoft.Xna.Framework;
+
+namespace FarseerPhysics.TestBed.Tests
+{
+    public class SphereBenchmarkTest : Test
+    {
+        private const int XCount = 30;
+        private const int YCount = 15;
+
+        private SphereBenchmarkTest()
+        {
+            Body ground = BodyFactory.CreateBody(World);
+
+            // Floor
+            Vertices edge = PolygonTools.CreateEdge(new Vector2(-40.0f, 0.0f), new Vector2(40.0f, 0.0f));
+            PolygonShape ashape = new PolygonShape(edge, 0);
+
+            ground.CreateFixture(ashape);
+
+            // Left wall
+            ashape.Set(PolygonTools.CreateEdge(new Vector2(-40.0f, 0.0f), new Vector2(-40.0f, 45.0f)));
+            ground.CreateFixture(ashape);
+
+            // Right wall
+            ashape.Set(PolygonTools.CreateEdge(new Vector2(40.0f, 0.0f), new Vector2(40.0f, 45.0f)));
+            ground.CreateFixture(ashape);
+
+            // Roof
+            ashape.Set(PolygonTools.CreateEdge(new Vector2(-40.0f, 45.0f), new Vector2(40.0f, 45.0f)));
+            ground.CreateFixture(ashape);
+
+            CircleShape shape = new CircleShape(1.0f, 1);
+
+            for (int i = 0; i < XCount; i++)
+            {
+                for (int j = 0; j < YCount; ++j)
+                {
+                    Body body = BodyFactory.CreateBody(World);
+                    body.BodyType = BodyType.Dynamic;
+                    body.Position = new Vector2(-38f + 2.1f * i, 2.0f + 2.0f * j);
+
+                    body.CreateFixture(shape);
+                }
+            }
+        }
+
+        public static Test Create()
+        {
+            return new SphereBenchmarkTest();
+        }
+    }
+}
