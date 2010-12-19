c41a5d0df522e0c0ab91eb305e8812f5f588b987
Ian Qvist <Genbox@users.noreply.github.com>
2010-12-19 20:59:44 +0000

    Fixed filtering bug in VelocityLimitController Added filtering to Explosion - ExplosionTest in Testbed demos this by having the first column unaffected by the explosion. Small name changes to filtering

--- SourceFiles/Controllers/Controller.cs
+++ SourceFiles/Controllers/Controller.cs
@@ -31,36 +31,36 @@ namespace FarseerPhysics.Controllers
 
     public class ControllerFilter
     {
-        public ControllerType ControllerIgnores;
+        public ControllerType ControllerFlags;
 
         /// <summary>
         /// Ignores the controller. The controller has no effect on this body.
         /// </summary>
-        /// <param name="controller">The flags.</param>
+        /// <param name="controller">The controller type.</param>
         public void IgnoreController(ControllerType controller)
         {
-            ControllerIgnores |= controller;
+            ControllerFlags |= controller;
         }
 
         /// <summary>
         /// Restore the controller. The controller affects this body.
         /// </summary>
-        /// <param name="controller">The controller.</param>
+        /// <param name="controller">The controller type.</param>
         public void RestoreController(ControllerType controller)
         {
-            ControllerIgnores &= ~controller;
+            ControllerFlags &= ~controller;
         }
 
         /// <summary>
         /// Determines whether this body ignores the the specified controller.
         /// </summary>
-        /// <param name="controller">The flags.</param>
+        /// <param name="controller">The controller type.</param>
         /// <returns>
         /// 	<c>true</c> if the body has the specified flag; otherwise, <c>false</c>.
         /// </returns>
         public bool IsControllerIgnored(ControllerType controller)
         {
-            return (ControllerIgnores & controller) == controller;
+            return (ControllerFlags & controller) == controller;
         }
     }
 
