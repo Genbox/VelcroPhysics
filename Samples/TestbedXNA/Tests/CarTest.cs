2403aa0fc274e6aa510d7245d483eaa1eb055f16
Ian Qvist <Genbox@users.noreply.github.com>
2011-02-13 01:26:10 +0000

    Updated CarTest to latest revision Fixes to LineJoint FixtureFactory now returns the Fixture it attaches Fixed JointFactory.CreateRevoluteJoint()

--- Samples/TestbedXNA/Tests/CarTest.cs
+++ Samples/TestbedXNA/Tests/CarTest.cs
@@ -25,74 +25,232 @@
 
 using System;
 using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Common;
 using FarseerPhysics.Dynamics;
-using FarseerPhysics.Dynamics.Contacts;
 using FarseerPhysics.Dynamics.Joints;
 using FarseerPhysics.Factories;
 using FarseerPhysics.TestBed.Framework;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace FarseerPhysics.TestBed.Tests
 {
     public class CarTest : Test
     {
         private Body _car;
+
         private float _hz;
+        private float _speed;
+        private LineJoint _spring1;
+        private LineJoint _spring2;
         private Body _wheel1;
         private Body _wheel2;
-
         private float _zeta;
 
         private CarTest()
         {
-            _hz = 2.0f;
+            _hz = 4.0f;
             _zeta = 0.7f;
+            _speed = 50.0f;
 
-            BodyFactory.CreateEdge(World, new Vector2(-40.0f, 0.0f), new Vector2(40.0f, 0.0f));
+            Body ground = new Body(World);
+            {
+                EdgeShape shape = new EdgeShape(new Vector2(-20.0f, 0.0f), new Vector2(20.0f, 0.0f));
+                ground.CreateFixture(shape);
 
+                float[] hs = new[] { 0.25f, 1.0f, 4.0f, 0.0f, 0.0f, -1.0f, -2.0f, -2.0f, -1.25f, 0.0f };
+
+                float x = 20.0f, y1 = 0.0f;
+                const float dx = 5.0f;
+
+                for (int i = 0; i < 10; ++i)
+                {
+                    float y2 = hs[i];
+                    FixtureFactory.AttachEdge(new Vector2(x, y1), new Vector2(x + dx, y2), ground);
+                    y1 = y2;
+                    x += dx;
+                }
+
+                for (int i = 0; i < 10; ++i)
+                {
+                    float y2 = hs[i];
+                    FixtureFactory.AttachEdge(new Vector2(x, y1), new Vector2(x + dx, y2), ground);
+                    y1 = y2;
+                    x += dx;
+                }
+
+                FixtureFactory.AttachEdge(new Vector2(x, 0.0f), new Vector2(x + 40.0f, 0.0f), ground);
+
+                x += 80.0f;
+                FixtureFactory.AttachEdge(new Vector2(x, 0.0f), new Vector2(x + 40.0f, 0.0f), ground);
+
+                x += 40.0f;
+                FixtureFactory.AttachEdge(new Vector2(x, 0.0f), new Vector2(x + 10.0f, 5.0f), ground);
+
+                x += 20.0f;
+                FixtureFactory.AttachEdge(new Vector2(x, 0.0f), new Vector2(x + 40.0f, 0.0f), ground);
+
+                x += 40.0f;
+                FixtureFactory.AttachEdge(new Vector2(x, 0.0f), new Vector2(x, 20.0f), ground);
+
+                ground.Friction = 0.6f;
+            }
+
+            // Teeter
+            {
+                Body body = new Body(World);
+                body.BodyType = BodyType.Dynamic;
+                body.Position = new Vector2(140.0f, 1.0f);
+
+                PolygonShape box = new PolygonShape(1);
+                box.SetAsBox(10.0f, 0.25f);
+                body.CreateFixture(box);
+
+                FixedRevoluteJoint jd = JointFactory.CreateFixedRevoluteJoint(World, body, Vector2.Zero, body.Position);
+                jd.LowerLimit = -8.0f * Settings.Pi / 180.0f;
+                jd.UpperLimit = 8.0f * Settings.Pi / 180.0f;
+                jd.LimitEnabled = true;
+
+                body.ApplyAngularImpulse(100.0f);
+            }
+
+            //Bridge
+            {
+                const int N = 20;
+                PolygonShape shape = new PolygonShape(1);
+                shape.SetAsBox(1.0f, 0.125f);
+
+                Body prevBody = ground;
+                for (int i = 0; i < N; ++i)
+                {
+                    Body body = new Body(World);
+                    body.BodyType = BodyType.Dynamic;
+                    body.Position = new Vector2(161.0f + 2.0f * i, -0.125f);
+                    Fixture fix = body.CreateFixture(shape);
+                    fix.Friction = 0.6f;
+
+                    Vector2 anchor = new Vector2(160.0f + 2.0f * i, -0.125f);
+                    JointFactory.CreateRevoluteJoint(World, prevBody, body, anchor);
+
+                    prevBody = body;
+                }
+
+                Vector2 anchor2 = new Vector2(160.0f + 2.0f * N, -0.125f);
+                JointFactory.CreateRevoluteJoint(World, prevBody, ground, anchor2);
+            }
+
+            // Boxes
             {
                 PolygonShape box = new PolygonShape(1);
-                box.SetAsBox(2.0f, 0.5f);
+                box.SetAsBox(0.5f, 0.5f);
 
-                CircleShape circle = new CircleShape(0.5f, 1);
+                Body body = new Body(World);
+                body.BodyType = BodyType.Dynamic;
+                body.Position = new Vector2(220.0f, 0.5f);
+                body.CreateFixture(box);
+
+                body = new Body(World);
+                body.BodyType = BodyType.Dynamic;
+                body.Position = new Vector2(220.0f, 1.5f);
+                body.CreateFixture(box);
+
+                body = new Body(World);
+                body.BodyType = BodyType.Dynamic;
+                body.Position = new Vector2(220.0f, 2.5f);
+                body.CreateFixture(box);
+            }
+
+            // Car
+            {
+                Vertices vertices = new Vertices(8);
+                vertices.Add(new Vector2(-1.5f, -0.5f));
+                vertices.Add(new Vector2(1.5f, -0.5f));
+                vertices.Add(new Vector2(1.5f, 0.0f));
+                vertices.Add(new Vector2(0.0f, 0.9f));
+                vertices.Add(new Vector2(-1.15f, 0.9f));
+                vertices.Add(new Vector2(-1.5f, 0.2f));
+
+                PolygonShape chassis = new PolygonShape(vertices, 1);
+
+                CircleShape circle = new CircleShape(0.4f, 1);
 
                 _car = new Body(World);
                 _car.BodyType = BodyType.Dynamic;
                 _car.Position = new Vector2(0.0f, 1.0f);
-                _car.CreateFixture(box);
+                _car.CreateFixture(chassis);
 
                 _wheel1 = new Body(World);
                 _wheel1.BodyType = BodyType.Dynamic;
-                _wheel1.Position = new Vector2(-1.5f, 0.5f);
-                _wheel1.CreateFixture(circle);
+                _wheel1.Position = new Vector2(-1.0f, 0.35f);
+                Fixture fix = _wheel1.CreateFixture(circle);
+                fix.Friction = 0.9f;
 
                 _wheel2 = new Body(World);
                 _wheel2.BodyType = BodyType.Dynamic;
-                _wheel2.Position = new Vector2(1.5f, 0.5f);
+                _wheel2.Position = new Vector2(1.0f, 0.4f);
                 _wheel2.CreateFixture(circle);
 
                 Vector2 axis = new Vector2(0.0f, 1.0f);
-                LineJoint jd = new LineJoint(_car, _wheel1, _wheel1.Position, axis);
-                jd.MotorSpeed = 1.0f;
-                jd.MaxMotorTorque = 10.0f;
-                jd.MotorEnabled = true;
-                jd.FrequencyHz = _hz;
-                jd.DampingRatio = _zeta;
-                World.AddJoint(jd);
+                _spring1 = new LineJoint(_car, _wheel1, _wheel1.Position, axis);
+                _spring1.MotorSpeed = 0.0f;
+                _spring1.MaxMotorTorque = 20.0f;
+                _spring1.MotorEnabled = true;
+                _spring1.FrequencyHz = _hz;
+                _spring1.DampingRatio = _zeta;
+                World.AddJoint(_spring1);
 
-                LineJoint jd2 = new LineJoint(_car, _wheel2, _wheel2.Position, axis);
-                jd2.MotorSpeed = 0.0f;
-                jd2.MaxMotorTorque = 10.0f;
-                jd2.MotorEnabled = false;
-                jd2.FrequencyHz = _hz;
-                jd2.DampingRatio = _zeta;
-                World.AddJoint(jd2);
+                _spring2 = new LineJoint(_car, _wheel2, _wheel2.Position, axis);
+                _spring2.MotorSpeed = 0.0f;
+                _spring2.MaxMotorTorque = 10.0f;
+                _spring2.MotorEnabled = false;
+                _spring2.FrequencyHz = _hz;
+                _spring2.DampingRatio = _zeta;
+                World.AddJoint(_spring2);
             }
         }
 
-        private bool On(Fixture fixturea, Fixture fixtureb, Contact contact)
+        public override void Keyboard(KeyboardManager keyboardManager)
         {
-            return true;
+            if (keyboardManager.IsNewKeyPress(Keys.A))
+            {
+                _spring1.MotorSpeed = _speed;
+            }
+            else if (keyboardManager.IsNewKeyPress(Keys.S))
+            {
+                _spring1.MotorSpeed = 0.0f;
+            }
+            else if (keyboardManager.IsNewKeyPress(Keys.D))
+            {
+                _spring1.MotorSpeed = -_speed;
+            }
+            else if (keyboardManager.IsNewKeyPress(Keys.Q))
+            {
+                _hz = Math.Max(0.0f, _hz - 1.0f);
+                _spring1.FrequencyHz = _hz;
+                _spring2.FrequencyHz = _hz;
+            }
+            else if (keyboardManager.IsNewKeyPress(Keys.E))
+            {
+                _hz += 1.0f;
+                _spring1.FrequencyHz = _hz;
+                _spring2.FrequencyHz = _hz;
+            }
+
+            base.Keyboard(keyboardManager);
+        }
+
+        public override void Update(GameSettings settings, GameTime gameTime)
+        {
+            //m_debugDraw.DrawString(5, m_textLine, "Keys: left = a, brake = s, right = d, toggle motor = m");
+            //m_textLine += 15;
+            //m_debugDraw.DrawString(5, m_textLine, "frequency = %g hz, damping ratio = %g", m_hz, m_zeta);
+            //m_textLine += 15;
+            //m_debugDraw.DrawString(5, m_textLine, "actual speed = %g rad/sec", m_spring1.GetJointSpeed());
+            //m_textLine += 15;
+
+            GameInstance.ViewCenter = _car.Position;
+
+            base.Update(settings, gameTime);
         }
 
         internal static Test Create()
--- SourceFiles/Factories/FixtureFactory.cs
+++ SourceFiles/Factories/FixtureFactory.cs
@@ -13,91 +13,91 @@ namespace FarseerPhysics.Factories
     /// </summary>
     public static class FixtureFactory
     {
-        public static void AttachEdge(Vector2 start, Vector2 end, Body body)
+        public static Fixture AttachEdge(Vector2 start, Vector2 end, Body body)
         {
-            AttachEdge(start, end, body, null);
+            return AttachEdge(start, end, body, null);
         }
 
-        public static void AttachEdge(Vector2 start, Vector2 end, Body body, object userData)
+        public static Fixture AttachEdge(Vector2 start, Vector2 end, Body body, object userData)
         {
             EdgeShape edgeShape = new EdgeShape(start, end);
-            body.CreateFixture(edgeShape, userData);
+            return body.CreateFixture(edgeShape, userData);
         }
 
-        public static void AttachLoopShape(Vertices vertices, Body body)
+        public static Fixture AttachLoopShape(Vertices vertices, Body body)
         {
-            AttachLoopShape(vertices, body, null);
+            return AttachLoopShape(vertices, body, null);
         }
 
-        public static void AttachLoopShape(Vertices vertices, Body body, object userData)
+        public static Fixture AttachLoopShape(Vertices vertices, Body body, object userData)
         {
             LoopShape shape = new LoopShape(vertices);
-            body.CreateFixture(shape, userData);
+            return body.CreateFixture(shape, userData);
         }
 
-        public static void AttachRectangle(float width, float height, float density, Vector2 offset, Body body,
+        public static Fixture AttachRectangle(float width, float height, float density, Vector2 offset, Body body,
                                               object userData)
         {
             Vertices rectangleVertices = PolygonTools.CreateRectangle(width / 2, height / 2);
             rectangleVertices.Translate(ref offset);
             PolygonShape rectangleShape = new PolygonShape(rectangleVertices, density);
-            body.CreateFixture(rectangleShape, userData);
+            return body.CreateFixture(rectangleShape, userData);
         }
 
-        public static void AttachRectangle(float width, float height, float density, Vector2 offset, Body body)
+        public static Fixture AttachRectangle(float width, float height, float density, Vector2 offset, Body body)
         {
-            AttachRectangle(width, height, density, offset, body, null);
+            return AttachRectangle(width, height, density, offset, body, null);
         }
 
-        public static void AttachCircle(float radius, float density, Body body)
+        public static Fixture AttachCircle(float radius, float density, Body body)
         {
-            AttachCircle(radius, density, body, null);
+            return AttachCircle(radius, density, body, null);
         }
 
-        public static void AttachCircle(float radius, float density, Body body, object userData)
+        public static Fixture AttachCircle(float radius, float density, Body body, object userData)
         {
             if (radius <= 0)
                 throw new ArgumentOutOfRangeException("radius", "Radius must be more than 0 meters");
 
             CircleShape circleShape = new CircleShape(radius, density);
-            body.CreateFixture(circleShape, userData);
+            return body.CreateFixture(circleShape, userData);
         }
 
-        public static void AttachCircle(float radius, float density, Body body, Vector2 offset)
+        public static Fixture AttachCircle(float radius, float density, Body body, Vector2 offset)
         {
-            AttachCircle(radius, density, body, offset, null);
+            return AttachCircle(radius, density, body, offset, null);
         }
 
-        public static void AttachCircle(float radius, float density, Body body, Vector2 offset, object userData)
+        public static Fixture AttachCircle(float radius, float density, Body body, Vector2 offset, object userData)
         {
             if (radius <= 0)
                 throw new ArgumentOutOfRangeException("radius", "Radius must be more than 0 meters");
 
             CircleShape circleShape = new CircleShape(radius, density);
             circleShape.Position = offset;
-            body.CreateFixture(circleShape, userData);
+            return body.CreateFixture(circleShape, userData);
         }
 
-        public static void AttachPolygon(Vertices vertices, float density, Body body)
+        public static Fixture AttachPolygon(Vertices vertices, float density, Body body)
         {
-            AttachPolygon(vertices, density, body, null);
+            return AttachPolygon(vertices, density, body, null);
         }
 
-        public static void AttachPolygon(Vertices vertices, float density, Body body, object userData)
+        public static Fixture AttachPolygon(Vertices vertices, float density, Body body, object userData)
         {
             if (vertices.Count <= 1)
                 throw new ArgumentOutOfRangeException("vertices", "Too few points to be a polygon");
 
             PolygonShape polygon = new PolygonShape(vertices, density);
-            body.CreateFixture(polygon, userData);
+            return body.CreateFixture(polygon, userData);
         }
 
-        public static void AttachEllipse(float xRadius, float yRadius, int edges, float density, Body body)
+        public static Fixture AttachEllipse(float xRadius, float yRadius, int edges, float density, Body body)
         {
-            AttachEllipse(xRadius, yRadius, edges, density, body, null);
+            return AttachEllipse(xRadius, yRadius, edges, density, body, null);
         }
 
-        public static void AttachEllipse(float xRadius, float yRadius, int edges, float density, Body body,
+        public static Fixture AttachEllipse(float xRadius, float yRadius, int edges, float density, Body body,
                                             object userData)
         {
             if (xRadius <= 0)
@@ -108,30 +108,33 @@ namespace FarseerPhysics.Factories
 
             Vertices ellipseVertices = PolygonTools.CreateEllipse(xRadius, yRadius, edges);
             PolygonShape polygonShape = new PolygonShape(ellipseVertices, density);
-            body.CreateFixture(polygonShape, userData);
+            return body.CreateFixture(polygonShape, userData);
         }
-        public static void AttachCompoundPolygon(List<Vertices> list, float density, Body body)
+        public static List<Fixture> AttachCompoundPolygon(List<Vertices> list, float density, Body body)
         {
-            AttachCompoundPolygon(list, density, body, null);
+            return AttachCompoundPolygon(list, density, body, null);
         }
 
-        public static void AttachCompoundPolygon(List<Vertices> list, float density, Body body, object userData)
+        public static List<Fixture> AttachCompoundPolygon(List<Vertices> list, float density, Body body, object userData)
         {
+            List<Fixture> res = new List<Fixture>(list.Count);
+
             //Then we create several fixtures using the body
             foreach (Vertices vertices in list)
             {
                 if (vertices.Count == 2)
                 {
                     EdgeShape shape = new EdgeShape(vertices[0], vertices[1]);
-                    body.CreateFixture(shape, userData);
+                    res.Add(body.CreateFixture(shape, userData));
                 }
                 else
                 {
                     PolygonShape shape = new PolygonShape(vertices, density);
-                    body.CreateFixture(shape, userData);
+                    res.Add(body.CreateFixture(shape, userData));
                 }
             }
-        }
 
+            return res;
+        }
     }
 }
