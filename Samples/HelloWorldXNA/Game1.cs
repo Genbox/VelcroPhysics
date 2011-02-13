97674afd3df9a086fca0cc4e30cdbf2cb6728cff
genbox_cp <SND\genbox_cp>
2011-02-13 12:31:16 +0000

    Simplified HelloWorld a bit by removing DebugView and camera rotation

--- Samples/HelloWorldXNA/Game1.cs
+++ Samples/HelloWorldXNA/Game1.cs
@@ -1,4 +1,3 @@
-using FarseerPhysics.DebugViews;
 using FarseerPhysics.Dynamics;
 using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
@@ -18,34 +17,28 @@ namespace FarseerPhysics.HelloWorld
         private SpriteFont _font;
         private bool _showDebug;
 
-        // Toogle DebugView with F1
         private World _world;
-        private DebugViewXNA _debugView;
 
         private Body _circleBody;
         private Body _groundBody;
 
         private Texture2D _circleSprite;
         private Texture2D _groundSprite;
-        private Matrix _projection;
 
         // Simple camera controls
         private Matrix _view;
-        private Matrix _viewDebug;
         private Vector2 _cameraPosition;
         private Vector2 _screenCenter;
-        private float _cameraRotation;
 
-        const string Text = "Press F1 to toogle the DebugView\n" +
-                            "Press A or D to rotate the ball\n" +
+        const string Text = "Press A or D to rotate the ball\n" +
                             "Press Space to jump\n" +
                             "Press Shift + W/S/A/D to move the camera\n" +
                             "Press Shift + Q/E to rotate the camera";
 
         // Farseer expects objects to be scaled to MKS (meters, kilos, seconds)
-        // 1m equals 64px here
+        // 1 meters equals 64 pixels here
         // (Objects should be scaled to be between 0.1 and 10 meters in size)
-        private const float MeterInPx = 64f;
+        private const float MeterInPixels = 64f;
 
         public Game1()
         {
@@ -56,8 +49,6 @@ namespace FarseerPhysics.HelloWorld
             Content.RootDirectory = "Content";
 
             _world = new World(new Vector2(0, 20));
-            _debugView = new DebugViewXNA(_world);
-            _showDebug = false;
         }
 
         /// <summary>
@@ -66,14 +57,9 @@ namespace FarseerPhysics.HelloWorld
         /// </summary>
         protected override void LoadContent()
         {
-            // The DebugView needs a projection matrix with the screen size in meters
-            _projection = Matrix.CreateOrthographicOffCenter(0f, _graphics.GraphicsDevice.Viewport.Width / MeterInPx,
-                                                             _graphics.GraphicsDevice.Viewport.Height / MeterInPx, 0f, 0f, 1f);
             // Initialize camera controls
             _view = Matrix.Identity;
-            _viewDebug = Matrix.Identity;
             _cameraPosition = Vector2.Zero;
-            _cameraRotation = 0f;
 
             _screenCenter = new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2f,
                                                 _graphics.GraphicsDevice.Viewport.Height / 2f);
@@ -81,33 +67,28 @@ namespace FarseerPhysics.HelloWorld
             _batch = new SpriteBatch(_graphics.GraphicsDevice);
             _font = Content.Load<SpriteFont>("font");
 
-            _debugView.LoadContent(_graphics.GraphicsDevice, Content);
-
             // Load sprites
             _circleSprite = Content.Load<Texture2D>("circleSprite"); //  96px x 96px => 1.5m x 1.5m
             _groundSprite = Content.Load<Texture2D>("groundSprite"); // 512px x 64px =>   8m x 1m
 
-            /* Circle Fixture: */
+            /* Circle */
             // Convert screen center from pixels to meters
-            Vector2 circlePosition = _screenCenter / MeterInPx;
-            // Add circle 1.5 meters above the screen center
-            circlePosition -= Vector2.UnitY * 1.5f;
+            Vector2 circlePosition = (_screenCenter / MeterInPixels) + new Vector2(0, -1.5f);
 
             // Create the circle fixture
-            _circleBody = BodyFactory.CreateCircle(_world, 96f / (2f * MeterInPx), 1f, circlePosition);
+            _circleBody = BodyFactory.CreateCircle(_world, 96f / (2f * MeterInPixels), 1f, circlePosition);
             _circleBody.BodyType = BodyType.Dynamic;
 
             // Give it some bounce and friction
             _circleBody.Restitution = 0.3f;
             _circleBody.Friction = 0.5f;
 
-            /* Ground Fixture: */
-            Vector2 groundPosition = _screenCenter / MeterInPx + Vector2.UnitY * 1.25f;
+            /* Ground */
+            Vector2 groundPosition = (_screenCenter / MeterInPixels) + new Vector2(0, 1.25f);
 
             // Create the ground fixture
-            _groundBody = BodyFactory.CreateRectangle(_world, 512f / MeterInPx, 64f / MeterInPx, 1f, groundPosition);
+            _groundBody = BodyFactory.CreateRectangle(_world, 512f / MeterInPixels, 64f / MeterInPixels, 1f, groundPosition);
             _groundBody.IsStatic = true;
-
             _groundBody.Restitution = 0.3f;
             _groundBody.Friction = 0.5f;
         }
@@ -137,19 +118,8 @@ namespace FarseerPhysics.HelloWorld
                 if (state.IsKeyDown(Keys.S))
                     _cameraPosition.Y -= 1.5f;
 
-                // Rotate camera
-                if (state.IsKeyDown(Keys.Q))
-                    _cameraRotation -= 0.05f;
-
-                if (state.IsKeyDown(Keys.E))
-                    _cameraRotation += 0.05f;
-
                 _view = Matrix.CreateTranslation(new Vector3(_cameraPosition - _screenCenter, 0f)) *
-                        Matrix.CreateRotationZ(_cameraRotation) *
                         Matrix.CreateTranslation(new Vector3(_screenCenter, 0f));
-                _viewDebug = Matrix.CreateTranslation(new Vector3((_cameraPosition - _screenCenter) / MeterInPx, 0f)) *
-                             Matrix.CreateRotationZ(_cameraRotation) *
-                             Matrix.CreateTranslation(new Vector3(_screenCenter / MeterInPx, 0f));
             }
             else
             {
@@ -188,12 +158,12 @@ namespace FarseerPhysics.HelloWorld
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             /* Circle position and rotation */
-            // Convert physics position to screen coordinates
-            Vector2 circlePos = _circleBody.Position * MeterInPx;
+            // Convert physics position (meters) to screen coordinates (pixels)
+            Vector2 circlePos = _circleBody.Position * MeterInPixels;
             float circleRotation = _circleBody.Rotation;
 
             /* Ground position and origin */
-            Vector2 groundPos = _groundBody.Position * MeterInPx;
+            Vector2 groundPos = _groundBody.Position * MeterInPixels;
             Vector2 groundOrigin = new Vector2(_groundSprite.Width / 2f, _groundSprite.Height / 2f);
 
             // Align sprite center to body position
@@ -216,11 +186,6 @@ namespace FarseerPhysics.HelloWorld
             _batch.DrawString(_font, Text, new Vector2(12f, 12f), Color.White);
 
             _batch.End();
-
-            if (_showDebug)
-            {
-                _debugView.RenderDebugData(ref _projection, ref _viewDebug);
-            }
 
             base.Draw(gameTime);
         }
