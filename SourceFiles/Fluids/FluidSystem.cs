8862ba8ab8469497c5161325a361eea6f31077ed
Ian Qvist <Genbox@users.noreply.github.com>
2012-12-21 04:52:16 +0000

    Added SPH timing to FPE and DebugView Experiments with performance improvements in SPH Switched to SpriteBatch instead of PrimitiveBatch since it can take all the particles better.

--- SourceFiles/Fluids/FluidSystem.cs
+++ SourceFiles/Fluids/FluidSystem.cs
@@ -116,72 +116,131 @@ namespace FarseerPhysics.Fluids
             }
         }
 
-        private void DoubleDensityRelaxation(FluidParticle p, float timeStep)
+        private const int MaxNeighbors = 25;
+        //private int _len2;
+        //private int _j;
+        //private float _q;
+        //private float _qq;
+
+        //private Vector2 _rij;
+        //private float _d;
+        //private Vector2 _dx;
+        private float _density;
+        private float _densityNear;
+        private float _pressure;
+        private float _pressureNear;
+        private float[] _distanceCache = new float[MaxNeighbors];
+        private float _timeStep2;
+
+        //private void DoubleDensityRelaxation1(FluidParticle p, float timeStep)
+        //{
+        //    _density = 0;
+        //    _densityNear = 0;
+
+        //    _len2 = p.Neighbours.Count;
+        //    if (_len2 > MaxNeighbors)
+        //        _len2 = MaxNeighbors;
+
+        //    for (_j = 0; _j < _len2; _j++)
+        //    {
+        //        _q = Vector2.DistanceSquared(p.Position, p.Neighbours[_j].Position);
+        //        _distanceCache[_j] = _q;
+        //        if (_q < _influenceRadiusSquared && _q != 0)
+        //        {
+        //            _q = (float)Math.Sqrt(_q);
+        //            _q /= Definition.InfluenceRadius;
+        //            _qq = ((1 - _q) * (1 - _q));
+        //            _density += _qq;
+        //            _densityNear += _qq * (1 - _q);
+        //        }
+        //    }
+
+        //    _pressure = Definition.Stiffness * (_density - Definition.DensityRest);
+        //    _pressureNear = Definition.StiffnessNear * _densityNear;
+
+        //    _dx = Vector2.Zero;
+
+        //    for (_j = 0; _j < _len2; _j++)
+        //    {
+        //        _q = _distanceCache[_j];
+        //        if (_q < _influenceRadiusSquared && _q != 0)
+        //        {
+        //            _q = (float)Math.Sqrt(_q);
+        //            _rij = p.Neighbours[_j].Position;
+        //            _rij -= p.Position;
+        //            _rij *= 1 / _q;
+        //            _q /= _influenceRadiusSquared;
+
+        //            _d = ((timeStep * timeStep) * (_pressure * (1 - _q) + _pressureNear * (1 - _q) * (1 - _q)));
+        //            _rij *= _d * 0.5f;
+        //            p.Neighbours[_j].Position += _rij;
+        //            _dx -= _rij;
+        //        }
+        //    }
+        //    p.Position += _dx;
+        //}
+
+        private void DoubleDensityRelaxation(FluidParticle particle)
         {
-            float density = 0.0f;
-            float densityNear = 0.0f;
+            _density = 0.0f;
+            _densityNear = 0.0f;
 
-            for (int i = 0; i < p.Neighbours.Count; ++i)
+            int neightborCount = particle.Neighbours.Count;
+
+            if (neightborCount > MaxNeighbors)
+                neightborCount = MaxNeighbors;
+
+            for (int i = 0; i < neightborCount; ++i)
             {
-                FluidParticle neighbour = p.Neighbours[i];
+                FluidParticle neighbour = particle.Neighbours[i];
 
-                if (p.Index == neighbour.Index)
-                {
+                if (particle.Index == neighbour.Index)
                     continue;
-                }
 
                 float q;
-                Vector2.DistanceSquared(ref p.Position, ref neighbour.Position, out q);
+                Vector2.DistanceSquared(ref particle.Position, ref neighbour.Position, out q);
+                _distanceCache[i] = q;
 
                 if (q > _influenceRadiusSquared)
-                {
                     continue;
-                }
 
                 q = 1.0f - (float)Math.Sqrt(q) / Definition.InfluenceRadius;
 
                 float densityDelta = q * q;
-                density += densityDelta;
-                densityNear += densityDelta * q;
+                _density += densityDelta;
+                _densityNear += densityDelta * q;
             }
 
-            float pressure = Definition.Stiffness * (density - Definition.DensityRest);
-            float pressureNear = Definition.StiffnessNear * densityNear;
+            _pressure = Definition.Stiffness * (_density - Definition.DensityRest);
+            _pressureNear = Definition.StiffnessNear * _densityNear;
 
             // For gameplay purposes
-            p.Density = density + densityNear;
-            p.Pressure = pressure + pressureNear;
+            particle.Density = _density + _densityNear;
+            particle.Pressure = _pressure + _pressureNear;
 
             Vector2 delta = Vector2.Zero;
 
-            for (int i = 0; i < p.Neighbours.Count; ++i)
+            for (int i = 0; i < neightborCount; ++i)
             {
-                FluidParticle neighbour = p.Neighbours[i];
+                FluidParticle neighbour = particle.Neighbours[i];
 
-                if (p.Index == neighbour.Index)
-                {
+                if (particle.Index == neighbour.Index)
                     continue;
-                }
 
-                float q;
-                Vector2.DistanceSquared(ref p.Position, ref neighbour.Position, out q);
+                float q = _distanceCache[i];
 
                 if (q > _influenceRadiusSquared)
-                {
                     continue;
-                }
 
                 q = 1.0f - (float)Math.Sqrt(q) / Definition.InfluenceRadius;
 
-                float dispFactor = 0.5f * timeStep * timeStep * (q * (pressure + pressureNear * q));
+                float dispFactor = _timeStep2 * (q * (_pressure + _pressureNear * q));
 
                 Vector2 direction;
-                Vector2.Subtract(ref neighbour.Position, ref p.Position, out direction);
+                Vector2.Subtract(ref neighbour.Position, ref particle.Position, out direction);
 
                 if (direction.LengthSquared() < float.Epsilon)
-                {
                     continue;
-                }
 
                 direction.Normalize();
 
@@ -194,7 +253,7 @@ namespace FarseerPhysics.Fluids
                 Vector2.Add(ref delta, ref disp, out delta);
             }
 
-            Vector2.Add(ref p.Position, ref delta, out p.Position);
+            Vector2.Add(ref particle.Position, ref delta, out particle.Position);
         }
 
         private void CreateSprings(FluidParticle p)
@@ -283,6 +342,8 @@ namespace FarseerPhysics.Fluids
 
         public void Update(float timeStep)
         {
+            _timeStep2 = 0.5f * timeStep * timeStep;
+
             ComputeNeighbours();
             ApplyForces();
 
@@ -312,7 +373,7 @@ namespace FarseerPhysics.Fluids
                 FluidParticle p = Particles[i];
                 if (p.IsActive)
                 {
-                    DoubleDensityRelaxation(p, timeStep);
+                    DoubleDensityRelaxation(p);
                 }
             }
 
