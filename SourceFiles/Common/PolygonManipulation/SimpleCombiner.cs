9506540f02462d0005044623e2f79877dbb12139
Ian Qvist <Genbox@users.noreply.github.com>
2013-01-24 03:46:58 +0000

    More work on the triangulation algorithms.

--- SourceFiles/Common/PolygonManipulation/SimpleCombiner.cs
+++ SourceFiles/Common/PolygonManipulation/SimpleCombiner.cs
@@ -1,4 +1,24 @@
-﻿using System.Collections.Generic;
+﻿/*
+* C# Version Ported by Matt Bettcher and Ian Qvist 2009-2010
+* 
+* Original C++ Version Copyright (c) 2007 Eric Jordan
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace FarseerPhysics.Common.PolygonManipulation
@@ -119,6 +139,7 @@ namespace FarseerPhysics.Common.PolygonManipulation
                 }
             }
 
+            //TODO: Add sanity check
             //Remove empty vertice collections
             for (int i = polys.Count - 1; i >= 0; i--)
             {
--- SourceFiles/Settings.cs
+++ SourceFiles/Settings.cs
@@ -43,6 +43,13 @@ namespace FarseerPhysics
         public const bool EnableDiagnostics = true;
 
         /// <summary>
+        /// Set this to true to skip sanity checks in the engine. This will speed up the
+        /// tools by removing the overhead of the checks, but you will need to handle checks
+        /// yourself where it is needed.
+        /// </summary>
+        public const bool SkipSanityChecks = false;
+
+        /// <summary>
         /// The number of velocity iterations used in the solver.
         /// </summary>
         public static int VelocityIterations = 8;
