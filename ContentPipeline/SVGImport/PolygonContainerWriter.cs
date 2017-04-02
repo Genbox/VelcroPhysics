74a85620f2b3afd701e97a9aee96dc2bc02c3bc0
Ian Qvist <qvistian@gmail.com>
2017-04-02 21:56:47 +0200

    Added SVGImport CPE

---
+++ ContentPipeline/SVGImport/PolygonContainerWriter.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+namespace VelcroPhysics.ContentPipeline.SVGImport
+{
+    [ContentTypeWriter]
+    public class PolygonContainerWriter : ContentTypeWriter<PolygonContainer>
+    {
+        protected override void Write(ContentWriter output, PolygonContainer container)
+        {
+            output.Write(container.Count);
+            foreach (KeyValuePair<string, Polygon> p in container)
+            {
+                output.Write(p.Key);
+                output.Write(p.Value.Closed);
+                output.Write(p.Value.Vertices.Count);
+                foreach (Vector2 vec in p.Value.Vertices)
+                {
+                    output.Write(vec);
+                }
+            }
+        }
+
+        public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        {
+            return typeof(PolygonContainerReader).AssemblyQualifiedName;
+        }
+    }
+}
---
+++ ContentPipeline/SVGImport/RawBodyTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using VelcroPhysics.Dynamics;
+
+namespace VelcroPhysics.ContentPipeline.SVGImport
+{
+    public struct RawFixtureTemplate
+    {
+        public string Path;
+        public string Name;
+        public Matrix Transformation;
+        public float Density;
+        public float Friction;
+        public float Restitution;
+    }
+
+    public struct RawBodyTemplate
+    {
+        public List<RawFixtureTemplate> Fixtures;
+        public string Name;
+        public float Mass;
+        public BodyType BodyType;
+    }
+}
