47c7b12297ada978555b8af9a96bbc17d6b8ca07
Ian Qvist <qvistian@gmail.com>
2017-04-07 02:20:35 +0200

    Removed useless parenthesis

--- SourceFiles/Common/PolygonManipulation/SimplifyTools.cs
+++ SourceFiles/Common/PolygonManipulation/SimplifyTools.cs
@@ -123,9 +123,9 @@ namespace VelcroPhysics.Common.PolygonManipulation
             //Gather points to process
             for (int i = 0; i < vertices.Count; ++i)
             {
-                int lower = (i == 0) ? (vertices.Count - 1) : (i - 1);
+                int lower = i == 0 ? vertices.Count - 1 : i - 1;
                 int middle = i;
-                int upper = (i == vertices.Count - 1) ? (0) : (i + 1);
+                int upper = i == vertices.Count - 1 ? 0 : i + 1;
 
                 float dx0 = vertices[middle].X - vertices[lower].X;
                 float dy0 = vertices[middle].Y - vertices[lower].Y;
