1934d6e4073d8f82f0a779975fca338e5bad47e9
Ian Qvist <qvistian@gmail.com>
2017-06-03 13:05:13 +0200

    Removed Serialization

--- VelcroPhysics/Tools/Serialization/XML/XMLFragmentAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace VelcroPhysics.Tools.Serialization.XML
-{
-    internal class XMLFragmentAttribute
-    {
-        public string Name { get; set; }
-        public string Value { get; set; }
-    }
-}
