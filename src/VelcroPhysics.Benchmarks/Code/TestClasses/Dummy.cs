94b6d65be615914b8aa637129b16a7e580b1b6c7
Ian Qvist <qvistian@gmail.com>
2021-04-18 22:49:58 +0200

    A benchmark to show overhead of method calls

--- src/VelcroPhysics.Benchmarks/Code/TestClasses/Dummy.cs
+++ src/VelcroPhysics.Benchmarks/Code/TestClasses/Dummy.cs
@@ -1,4 +1,13 @@
 ﻿namespace Genbox.VelcroPhysics.Benchmarks.Code.TestClasses
 {
-    public class Dummy { }
+    public class Dummy
+    {
+        public Struct32 ValueProperty { get; set; }
+        public Struct32 ValueField;
+
+        public Struct32 ValueMethod()
+        {
+            return ValueField;
+        }
+    }
 }
---
+++ src/VelcroPhysics.Benchmarks/Tests/CLR/FieldPropertyBenchmarks.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using Genbox.VelcroPhysics.Benchmarks.Code;
+using Genbox.VelcroPhysics.Benchmarks.Code.TestClasses;
+
+namespace Genbox.VelcroPhysics.Benchmarks.Tests.CLR
+{
+    public class FieldPropertyBenchmarks : UnmeasuredBenchmark
+    {
+        private readonly Dummy _dummy = new Dummy { ValueField = new Struct32() };
+
+        [Benchmark]
+        public Struct32 PropertyGetTest()
+        {
+            return _dummy.ValueProperty;
+        }
+
+        [Benchmark]
+        public Struct32 FieldGetTest()
+        {
+            return _dummy.ValueField;
+        }
+
+        [Benchmark]
+        public Struct32 MethodGetTest()
+        {
+            return _dummy.ValueMethod();
+        }
+    }
+}
