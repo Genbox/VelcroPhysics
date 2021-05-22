5054515411517d4647d3c16adaad2d540050751f
Ian Qvist <qvistian@gmail.com>
2021-05-22 21:42:05 +0200

    Add benchmark to test init of vector2 and an optimized version of aabb check

--- src/VelcroPhysics.Benchmarks/Tests/Primitives/AabbBenchmark.cs
+++ src/VelcroPhysics.Benchmarks/Tests/Primitives/AabbBenchmark.cs
@@ -2,6 +2,7 @@
 using Genbox.VelcroPhysics.Benchmarks.Code;
 using Genbox.VelcroPhysics.Collision.RayCast;
 using Genbox.VelcroPhysics.Shared;
+using Genbox.VelcroPhysics.Utilities;
 using Microsoft.Xna.Framework;
 
 namespace Genbox.VelcroPhysics.Benchmarks.Tests.Primitives
@@ -39,6 +40,40 @@ namespace Genbox.VelcroPhysics.Benchmarks.Tests.Primitives
             input.Point2 = _a.Center;
 
             return _a.RayCast(ref input, out _);
+        }
+
+
+        [Benchmark]
+        public AABB ComputeCircleAABBBenchmarkNormal()
+        {
+            Vector2 pos = new Vector2(1, 1);
+            float radius = 4f;
+            Transform tf = new Transform();
+
+            AABB aabb;
+            Vector2 p = tf.p + MathUtils.Mul(ref tf.q, pos);
+            aabb.LowerBound = new Vector2(p.X - radius, p.Y - radius);
+            aabb.UpperBound = new Vector2(p.X + radius, p.Y + radius);
+            return aabb;
+        }
+
+        [Benchmark]
+        public AABB ComputeCircleAABBBenchmarkOptimized()
+        {
+            Vector2 pos = new Vector2(1, 1);
+            float radius = 4f;
+            Transform tf = new Transform();
+
+            AABB aabb;
+            float posX = tf.p.X + (tf.q.c * pos.X - tf.q.s * pos.Y);
+            float posY = tf.p.Y + (tf.q.s * pos.X + tf.q.c * pos.Y);
+
+            aabb.LowerBound.X = posX - radius;
+            aabb.LowerBound.Y = posY - radius;
+            aabb.UpperBound.X = posX + radius;
+            aabb.UpperBound.Y = posY + radius;
+
+            return aabb;
         }
     }
 }
---
+++ src/VelcroPhysics.Benchmarks/Tests/Primitives/Vector2Benchmark.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using Genbox.VelcroPhysics.Benchmarks.Code;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+
+namespace Genbox.VelcroPhysics.Benchmarks.Tests.Primitives
+{
+    public class Vector2Benchmark : UnmeasuredBenchmark
+    {
+        [Benchmark]
+        public Vector2 CreateWithCtor()
+        {
+            return new Vector2(1, 2);
+        }
+
+        [Benchmark]
+        public Vector2 CreateWithNoInit()
+        {
+            Vector2 v;
+            v.X = 1;
+            v.Y = 2;
+            return v;
+        }
+
+        [Benchmark]
+        public Vector2 CreateWithZero()
+        {
+            Vector2 v = Vector2.Zero;
+            v.X = 1;
+            v.Y = 2;
+            return v;
+        }
+    }
+}
